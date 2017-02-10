11fbdca77b6c39cb679e8f77acd321da581135a2
fryntiz <tecnico@fryntiz.es>
2017-02-10 13:38:43 +0100

    Clase por defecto comentada

--- Aprendiendo/Arrays/src/arrays/Arrays.java
+++ Aprendiendo/Arrays/src/arrays/Arrays.java
@@ -1,21 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Clase por defecto (Por ahora no la uso)
+ * @author fryntiz
  */
 package arrays;
 
-/**
- *
- * @author fryntiz
- */
 public class Arrays {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
