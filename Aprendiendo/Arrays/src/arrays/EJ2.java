e7f6844f4cc354a2ff853f98adcd0229e2b9e536
fryntiz <tecnico@fryntiz.es>
2017-02-10 14:16:33 +0100

    Ejercicio 2 terminado Método que recibe array y devuelve solo los pares

--- Aprendiendo/Arrays/src/arrays/EJ2.java
+++ Aprendiendo/Arrays/src/arrays/EJ2.java
@@ -4,16 +4,24 @@ package arrays;
  * @author fryntiz
  */
 
-public class EJ2 {
-    static int[] arrayA = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-    static int[] arrayB;
+public class EJ2 { 
+    private static int[] arrayB = new int[8];
+    static int x = 0;
     
     public static void main(String[] args) {
+        int[] arrayA = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
         valoresPares(arrayA);
     }
     
-    public static void valoresPares(int x[]) {
-        
+    public static void valoresPares(int z[]) {//Calcula los números pares
+        System.out.println("Los números pares son:");
+        for (int i=0; i<=(z.length-1); i++) {
+            if ((z[i]%2) == 0) {
+                arrayB[x] = z[i];
+                System.out.print(arrayB[x] + ", ");
+                x++;
+            }
+        }
     }
     
 }
