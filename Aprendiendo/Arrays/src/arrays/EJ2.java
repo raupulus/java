54c0e811d3e00acbfdc8d3960132ee3f68d8c625
fryntiz <tecnico@fryntiz.es>
2017-02-10 13:44:58 +0100

    Base para ejercicio 2 planteada Creado 2 array y método donde trabajar

--- Aprendiendo/Arrays/src/arrays/EJ2.java
+++ Aprendiendo/Arrays/src/arrays/EJ2.java
@@ -5,5 +5,15 @@ package arrays;
  */
 
 public class EJ2 {
+    static int[] arrayA = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    static int[] arrayB;
+    
+    public static void main(String[] args) {
+        valoresPares(arrayA);
+    }
+    
+    public static void valoresPares(int x[]) {
+        
+    }
     
 }
