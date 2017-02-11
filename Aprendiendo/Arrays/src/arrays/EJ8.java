199b7e90629fa525e695dc22290666add679dde9
fryntiz <tecnico@fryntiz.es>
2017-02-11 23:44:28 +0100

    Método para llenar Array terminado

--- Aprendiendo/Arrays/src/arrays/EJ8.java
+++ Aprendiendo/Arrays/src/arrays/EJ8.java
@@ -5,15 +5,30 @@ package arrays;
  * el mayor, el menor, el que más se repite  y la media
  * @author fryntiz
  */
+import java.util.*; //Importo todas las librerías
 public class EJ8 {
     static int[] array = new int[100]; //Declaración del array con 100 posiciones
+    static Random rd = new Random();
+    public static int mayor,menor,repetido,media,tmp;
     
     public static void main(String[] args) {
         llenarArray();
+        sacarValores();
+        
+        for (int i=0; i<array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
     
     public static void llenarArray() { //Generar números aleatorios
-        
+        for (int i=0; i<array.length; i++) {
+            tmp = rd.nextInt(80);
+            if ((tmp>=10) && (tmp<=80)) {
+                array[i] = tmp;
+            } else {
+                i--;
+            }
+        }
     }
     
     public static void sacarValores() { //Sacar mayor, menor...
