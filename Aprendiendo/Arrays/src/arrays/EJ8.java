a0458118cf8782d25aa2eb2dbb8e5f395364c042
fryntiz <tecnico@fryntiz.es>
2017-02-11 23:32:28 +0100

    Estructura básica del programa

--- Aprendiendo/Arrays/src/arrays/EJ8.java
+++ Aprendiendo/Arrays/src/arrays/EJ8.java
@@ -1,10 +1,24 @@
 package arrays;
 /**
- *Realiza un programa que cree un vector de 100 posiciones con números aleatorios entre 10 y 80.
+ * Realiza un programa que cree un vector de 100 posiciones con números aleatorios entre 10 y 80.
  * Una vez creado el vector el programa deberá mostrar
  * el mayor, el menor, el que más se repite  y la media
  * @author fryntiz
  */
 public class EJ8 {
+    static int[] array = new int[100]; //Declaración del array con 100 posiciones
+    
+    public static void main(String[] args) {
+        llenarArray();
+    }
+    
+    public static void llenarArray() { //Generar números aleatorios
+        
+    }
+    
+    public static void sacarValores() { //Sacar mayor, menor...
+        
+    }
+    
     
 }
