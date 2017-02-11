f22411b903a08705161f1cc8fa43899deb896e1c
fryntiz <tecnico@fryntiz.es>
2017-02-11 20:12:11 +0100

    Ejercicio 7 terminado Pide 2 arrays, se unen, luego se ordenan y se muestran

--- Aprendiendo/Arrays/src/arrays/EJ7.java
+++ Aprendiendo/Arrays/src/arrays/EJ7.java
@@ -1,21 +1,37 @@
 package arrays;
-
 /**
  *Realiza un método que tome como parámetros de entrada dos arrays de enteros 
  * y devuelva como salida un único array con los elementos de los anteriores arrays ordenados de forma ascendente
  * @author fryntiz
  */
+import java.util.*; //Importo librerías
 public class EJ7 {
-    static int[] arrayA = {9,1,12,3,21};
-    static int[] arrayB = {13,8,6,44,31};
-    //static int[] arrayC = new int[10];
+    private static final Scanner sc = new Scanner(System.in);
     
-    public static void main(String[] args) {
-        int[] arrayC = ordenar(arrayA, arrayB);
+    public static void main(String[] args) { //Clase principal
+        System.out.println("*****PRIMER ARRAY*****");
+        int[] arrayA = input(); //Entrada arrayA
+        System.out.println("*****SEGUNDO ARRAY*****");
+        int[] arrayB = input(); //Entrada arrayB
+        int[] arrayC = ordenar(arrayA, arrayB); //Método que ordena los arrays
         
+        System.out.println("\n\nEl conjunto de los dos Arrays ordenados es:");
         for(int i=0;i<arrayC.length; i++) {
             System.out.println(arrayC[i]);
         }
+    }
+    
+    public static int[] input() {
+        System.out.println("Introduce la longitud del array");
+        int tmp = sc.nextInt(); //Longitud del array
+        int[] array = new int[tmp];
+        
+        for (int i=0; i<tmp; i++){
+            System.out.println("Introduce el valor " + i + " del array");
+            array[i] = sc.nextInt();
+        }
+        
+        return array; //Devuelve el array
     }
     
     public static int[] ordenar(int a[],int b[]) { //Método que mezcla en C los dos array y los ordena
@@ -42,6 +58,6 @@ public class EJ7 {
             }
         }
 
-        return arrayC;
+        return arrayC; //Devuelve el array C ordenado
     }
 }
