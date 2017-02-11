f47a85a6712d4c9c6527867db02d6934fe38923f
fryntiz <tecnico@fryntiz.es>
2017-02-11 19:03:55 +0100

    Declarada estructura básica para ordenar arrayA y arrayB

--- Aprendiendo/Arrays/src/arrays/EJ7.java
+++ Aprendiendo/Arrays/src/arrays/EJ7.java
@@ -1,9 +1,21 @@
 package arrays;
 
 /**
- *
+ *Realiza un método que tome como parámetros de entrada dos arrays de enteros 
+ * y devuelva como salida un único array con los elementos de los anteriores arrays ordenados de forma ascendente
  * @author fryntiz
  */
 public class EJ7 {
+    static int[] arrayA = {9,1,12,3,21};
+    static int[] arrayB = {9,1,12,3,21};
+    static int[] arrayC = new int[5];
     
+    public static void main(String[] args) {
+        ordenar(arrayA, arrayB, arrayC);
+    }
+    
+    public static int[] ordenar(int a[],int b[], int c[]) {
+        
+        return c;
+    }
 }
