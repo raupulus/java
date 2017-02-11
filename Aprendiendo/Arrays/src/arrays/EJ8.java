b82e2910620bb45b5fa0e2fa7afb87d2594bfca0
fryntiz <tecnico@fryntiz.es>
2017-02-11 23:28:44 +0100

    Descripción del problema

--- Aprendiendo/Arrays/src/arrays/EJ8.java
+++ Aprendiendo/Arrays/src/arrays/EJ8.java
@@ -1,6 +1,8 @@
 package arrays;
 /**
- *
+ *Realiza un programa que cree un vector de 100 posiciones con números aleatorios entre 10 y 80.
+ * Una vez creado el vector el programa deberá mostrar
+ * el mayor, el menor, el que más se repite  y la media
  * @author fryntiz
  */
 public class EJ8 {
