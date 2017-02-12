10363dcb75247f2153ca4e1930511ca76057d92c
fryntiz <tecnico@fryntiz.es>
2017-02-12 15:00:13 +0100

    Planteamiento básico de métodos para resolver el problema

--- Aprendiendo/Arrays/src/arrays/EJ9.java
+++ Aprendiendo/Arrays/src/arrays/EJ9.java
@@ -1,10 +1,28 @@
 package arrays;
 /**
- *Realiza un programa que cree un vector de 100 posiciones
+ * Realiza un programa que cree un vector de 100 posiciones
  * con números aleatorios entre 1 y 100. Una vez creado el vector se ordenará este
  * y mostrar los números entre 1 y 100 que no han sido almacenados.
  * @author fryntiz
  */
 public class EJ9 {
     
+    public static void main(String[] args) {
+        generarNumeros();
+        ordenar();
+        mostrar();
+        
+    }
+    
+    public static void generarNumeros() { //Llena el array de números aleatorios
+        
+    }
+    
+    public static void ordenar() { //Ordenar el array de menor a mayor
+        
+    }
+    
+    public static void mostrar() { //Dibuja el array por pantalla
+        
+    }
 }
