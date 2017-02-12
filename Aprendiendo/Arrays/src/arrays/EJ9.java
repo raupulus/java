4f1f1d611e4e5fdbc7eb228549735e3a1cf0e63e
fryntiz <tecnico@fryntiz.es>
2017-02-12 14:55:11 +0100

    Descripción del problema

--- Aprendiendo/Arrays/src/arrays/EJ9.java
+++ Aprendiendo/Arrays/src/arrays/EJ9.java
@@ -1,7 +1,8 @@
 package arrays;
-
 /**
- *
+ *Realiza un programa que cree un vector de 100 posiciones
+ * con números aleatorios entre 1 y 100. Una vez creado el vector se ordenará este
+ * y mostrar los números entre 1 y 100 que no han sido almacenados.
  * @author fryntiz
  */
 public class EJ9 {
