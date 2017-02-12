1aa5c7c8e033acfb2edd8b3c7d19ad844b89586c
fryntiz <tecnico@fryntiz.es>
2017-02-12 16:43:48 +0100

    Creado método para generar números

--- Aprendiendo/Arrays/src/arrays/EJ9.java
+++ Aprendiendo/Arrays/src/arrays/EJ9.java
@@ -5,17 +5,29 @@ package arrays;
  * y mostrar los números entre 1 y 100 que no han sido almacenados.
  * @author fryntiz
  */
+import java.util.*; //Añado todas las librerías
+
 public class EJ9 {
+    static Random rd = new Random();
+//x = rd.nextInt(1000); //numero máximo 1000
+    static int[] array = new int[100]; //Creo array de 100 posiciones
+    static int tmp;
     
     public static void main(String[] args) {
         generarNumeros();
         ordenar();
         mostrar();
-        
     }
     
     public static void generarNumeros() { //Llena el array de números aleatorios
-        
+        for(int i=0; i<array.length; i++) {
+            tmp = rd.nextInt(101);
+            if ((tmp<101) && (tmp>0)) {
+                array[i] = tmp;
+            } else {
+                i--;
+            }
+        }
     }
     
     public static void ordenar() { //Ordenar el array de menor a mayor
