cb5612310069b0432f4d9d190a1b7a2a2f85af81
fryntiz <tecnico@fryntiz.es>
2017-02-13 19:35:59 +0100

    Descripción del problema

--- Aprendiendo/Arrays/src/arrays/EJ13.java
+++ Aprendiendo/Arrays/src/arrays/EJ13.java
@@ -1,6 +1,7 @@
 package arrays;
 /**
- *
+ *Pedir al usuario los valores de 10 números y meterlos en un array y después
+ * mostrarlos en orden inverso a como fueron introducidos
  * @author fryntiz
  */
 public class EJ13 {
