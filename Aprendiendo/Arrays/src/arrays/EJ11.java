cc2a62c6f1ac9f42e86698b6e692845a406797d3
fryntiz <tecnico@fryntiz.es>
2017-02-13 19:06:18 +0100

    Planteamiento básico de estructura

--- Aprendiendo/Arrays/src/arrays/EJ11.java
+++ Aprendiendo/Arrays/src/arrays/EJ11.java
@@ -1,9 +1,33 @@
 package arrays;
 /**
- *
+ * Leer 10 números por teclado, almacenarlo en un array y mostrar la suma, resta,
+ * multiplicación y división de todos
  * @author fryntiz
  */
 import java.util.*; //Importo todas las librerías
 public class EJ11 {
     
+    public static void main(String[] args) {
+        entrada(); //Pide los 10 números
+    }
+    
+    public static void entrada() { //Pide los 10 números
+        
+    }
+    
+    public static void suma() {
+        
+    }
+    
+    public static void resta() {
+        
+    }
+    
+    public static void multiplicacion() {
+        
+    }
+    
+    public static void division() {
+        
+    }
 }
