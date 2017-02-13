b6e388cd428eedf0156b3c0d76b2adeef60f2c76
fryntiz <tecnico@fryntiz.es>
2017-02-13 18:08:55 +0100

    Planteamiento principal del problema

--- Aprendiendo/Arrays/src/arrays/Ej10.java
+++ Aprendiendo/Arrays/src/arrays/Ej10.java
@@ -1,11 +1,31 @@
 package arrays;
 /**
- * Define tres arrays de 20 números enteros cada una, con nopmbres, cuadrado y cubo
+ * Define tres arrays de 20 números enteros cada una, con nombres numero, cuadrado y cubo
  * Carga el array numero con valores aleatorios entre 0 y 100. En el array cuadrado
  * se deben almacenar los cuadrados de los valores que hay en un número.
  * A continuación, muestra el contenido de los tres arrays dispuesto en tres columnas.
  * @author fryntiz
  */
+import java.util.*; //Importo todas las librerías
 public class Ej10 {
+    int[] numeros,cuadrados,cubos = new int[20]; //Arrays
+    int tmp; //Variables
+    static Random rd = new Random();
+
     
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void llenarNumeros() {
+        //int tmp = rd.nextInt(100); //numero máximo 100
+    }
+    
+    public static void llenarCuadrados() {
+        
+    }
+    
+    public static void llenarCubos() {
+        
+    }
 }
