03cfb9bdece09a6b2194c369500b9089025745e9
fryntiz <tecnico@fryntiz.es>
2017-02-13 19:28:51 +0100

    metodología establecida

--- Aprendiendo/Arrays/src/arrays/EJ12.java
+++ Aprendiendo/Arrays/src/arrays/EJ12.java
@@ -3,13 +3,19 @@ package arrays;
  * Que rellene un array con los 100 primeros números impares y muestre la suma
  * @author fryntiz
  */
+import java.util.*;
 public class EJ12 {
+    static int[] array = new int[100];
+    private static Random rd = new Random();
     
     public static void main(String[] args) {
         
     }
     
     public static void rellenarArray() {
-        
+        for(int i = 0;i<array.length;i++) {
+            array[i] = rd.nextInt(100);
+            
+        }
     }
 }
