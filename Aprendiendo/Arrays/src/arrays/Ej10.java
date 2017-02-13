185aa2fb6a94ff2e6c6783381a647024e5a996c8
fryntiz <tecnico@fryntiz.es>
2017-02-13 17:55:10 +0100

    Descripción del problema

--- Aprendiendo/Arrays/src/arrays/Ej10.java
+++ Aprendiendo/Arrays/src/arrays/Ej10.java
@@ -1,6 +1,9 @@
 package arrays;
 /**
- *
+ * Define tres arrays de 20 números enteros cada una, con nopmbres, cuadrado y cubo
+ * Carga el array numero con valores aleatorios entre 0 y 100. En el array cuadrado
+ * se deben almacenar los cuadrados de los valores que hay en un número.
+ * A continuación, muestra el contenido de los tres arrays dispuesto en tres columnas.
  * @author fryntiz
  */
 public class Ej10 {
