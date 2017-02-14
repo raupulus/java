ed74380e57967dce8e80fddc94f93fa2ecaae550
fryntiz <tecnico@fryntiz.es>
2017-02-14 10:14:10 +0100

    Descripción del problema

--- Aprendiendo/Arrays/src/arrays/EJ16.java
+++ Aprendiendo/Arrays/src/arrays/EJ16.java
@@ -1,6 +1,8 @@
 package arrays;
 /**
- *
+ *Realiza un programa que pida la temperatura media que ha hecho en cada mes un determinado año
+ * y que muestre a continuación un diagrama de barras horizontales con esos datos.
+ * Las barras del diagrama se pueden dibujar a base de asteriscos o cualquier otro carácter.
  * @author fryntiz
  */
 public class EJ16 {
