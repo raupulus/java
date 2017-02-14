9eeff8c7bd6648efe31f573f540b2b0289bc480f
fryntiz <tecnico@fryntiz.es>
2017-02-14 01:18:11 +0100

    Planteado problema del ejercicio 14

--- Aprendiendo/Arrays/src/arrays/EJ14.java
+++ Aprendiendo/Arrays/src/arrays/EJ14.java
@@ -1,6 +1,14 @@
 package arrays;
 /**
- *
+ *En españa cada persona está identificada con un DNI, en el que figura un número y letra (56999545W
+ * La letra que sigue al número se calcula siguiendo esta metodología:
+ * Tomando el resto de dividir el número del DNI entre 23 (resultado es entre 0 y 22 siempre)
+ * Crear programa que  calcule la letra de un DNI a partir del que introduzca el usuario
+ * El programa devolverá el DNI completo (con letra)
+ * Crear un método obtenerLetra(int numeroDNI) donde según el resultado de la anterior
+ * fórmula busque en un array de caracteres la posición que corresponda con la letra
+ * La letra T equivale a 0, letra R a 1 y así hasta la letra E que equivale a 22
+ * Ejemplo: Para el DNI 20267079 el resto entre 23 es 8, sería la letra P
  * @author fryntiz
  */
 public class EJ14 {
