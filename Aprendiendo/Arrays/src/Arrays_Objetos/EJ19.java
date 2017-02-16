002787fb12723f53e2a9bde5bd272038aaa0109e
fryntiz <tecnico@fryntiz.es>
2017-02-16 20:25:12 +0100

    Descripción del problema para el ejercicio 19

--- Aprendiendo/Arrays/src/Arrays_Objetos/EJ19.java
+++ Aprendiendo/Arrays/src/Arrays_Objetos/EJ19.java
@@ -1,6 +1,13 @@
 package Arrays_Objetos;
 /**
- * 
+ * Desarrolla un programa que usando un menú permita gestionar el stock de una tienda de comestibles.
+ * La información a recoger será: id del producto, nombre del producto, precio, cantidad en stock.
+ * La tienda debe disponer de 10 productos distintos. El programa utilizará un menú del tipo:
+ * 1 - Dar de alta un producto nuevo
+ * 2 - Buscar producto por ID
+ * 3 - Modificar el stock de un producto
+ * 4 - Eliminar producto
+ * 5 - Salir del programa
  * @author fryntiz
  */
 public class EJ19 {
