7852f294603c2ffc856e5ceadd3eb34756f4ceb6
fryntiz <tecnico@fryntiz.es>
2017-02-16 17:32:31 +0100

    Descripción del problema

--- Aprendiendo/Arrays/src/Arrays_Objetos/EJ18.java
+++ Aprendiendo/Arrays/src/Arrays_Objetos/EJ18.java
@@ -1,6 +1,14 @@
 package Arrays_Objetos;
 /**
- *
+ * Diseñar una clase llamada Alumno con los atributos privados "nombre alumno" y "array con las 5 notas enteras de ese alumno"
+ * Crear los siguientes métodos públicos:
+ * -Un constructor que dimensiona el array e inicialice los atributos con los valores introducidos por teclado
+ * -Un método llamado mayor que devuelve la mejor nota
+ * -Otro método llamado menor que devuelve la menor de las notas
+ * -Mostrar que muestre el nombre del alumno, en mayúsculas, seguido de su nota media
+ * 
+ * Desde la clase principal declarar un objeto de tipo alumno y probar los métodos de la clase.
+ * 
  * @author fryntiz
  */
 public class EJ18 {
