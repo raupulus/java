d9f046aadc3688f5268e199f6680c7846d2399ca
fryntiz <tecnico@fryntiz.es>
2017-02-16 17:55:02 +0100

    Estructura básica

--- Aprendiendo/Arrays/src/Arrays_Objetos/alumno.java
+++ Aprendiendo/Arrays/src/Arrays_Objetos/alumno.java
@@ -4,5 +4,10 @@ package Arrays_Objetos;
  * @author fryntiz
  */
 public class alumno {
+    private static String nombre_alumno;
+    private static int[] notas = new int[5];
     
+    public static void alumno(int a,int b) {
+        
+    }
 }
