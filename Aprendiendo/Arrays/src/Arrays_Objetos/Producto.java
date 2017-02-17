5d74e2f13b954f754f6dd2e0f785cf63a13ee817
fryntiz <tecnico@fryntiz.es>
2017-02-17 16:19:37 +0100

    Cambio de nombre de clase

---
+++ Aprendiendo/Arrays/src/Arrays_Objetos/Producto.java
@@ -0,0 +1,21 @@
+package Arrays_Objetos;
+/**
+ * Clase complementaria del ejercicio 19
+ * Desde esta clase se crearán los productos
+ * @author fryntiz
+ */
+public class Producto {
+    private static int contador = 0,tmp;
+    private static String nombre;
+    
+    
+    public static void Producto(String nombreInput) {
+        nombre = nombreInput;
+        
+        
+    }
+    
+    public void newProducto() {
+        
+    }
+}
