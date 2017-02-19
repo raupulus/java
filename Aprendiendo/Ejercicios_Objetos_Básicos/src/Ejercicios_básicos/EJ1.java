54b235443f316aa9ae6117c078d3e90d9825a48a
fryntiz <tecnico@fryntiz.es>
2017-02-19 23:26:27 +0100

    Estructura en el método del menú para la acción elegida

--- Aprendiendo/Ejercicios_Objetos_Básicos/src/Ejercicios_básicos/EJ1.java
+++ Aprendiendo/Ejercicios_Objetos_Básicos/src/Ejercicios_básicos/EJ1.java
@@ -15,6 +15,7 @@ public class EJ1 {
     
     private static final Scanner sc = new Scanner(System.in);
     private static int input;
+    private static String pais;
     private static boolean salir;
 //input = sc.nextInt();
     
@@ -27,12 +28,37 @@ public class EJ1 {
     }
     
     public static void menu() {
+        salir=false;
         while (!salir) {
             System.out.println("***** MENÚ *****");
-            System.out.println("1 - Introducir paises");
+            System.out.println("1 - Introducir países");
             System.out.println("2 - Realizar sorteo");
             System.out.println("3 - Posición del país");
             System.out.println("4 - Salir");
+            input = sc.nextInt(); //Pedir acción del menú
+            switch (input) {
+                case 4:
+                    salir = true;
+                    break;
+                case 3:
+                    System.out.println("Introduce el país que desea conocer la posición:");
+                    pais = sc.nextLine();
+                    //Llamar a la clase en este punto pasandole como parámetro el pais
+                    break;
+                case 2:
+                    System.out.println("El país elegido por el sorteo es: ");
+                    
+                    break;
+                case 1:
+                    System.out.println("Introduce los países");
+                    for (int i=1;1<3;i++) {
+                        System.out.println("Introduce el país " + i);
+                    }
+                    //break;
+                default:
+                    System.out.println("No entiendo lo que escribes, inserta un valor válido");
+                    break;
+            }
         }
     }
 }
