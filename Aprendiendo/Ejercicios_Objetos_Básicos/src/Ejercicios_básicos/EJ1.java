18da4b6d2bcb1e87500be259a1bbe79a61cbafa0
fryntiz <tecnico@fryntiz.es>
2017-02-19 22:50:40 +0100

    Estructura básica para EJ1

--- Aprendiendo/Ejercicios_Objetos_Básicos/src/Ejercicios_básicos/EJ1.java
+++ Aprendiendo/Ejercicios_Objetos_Básicos/src/Ejercicios_básicos/EJ1.java
@@ -5,8 +5,18 @@ package Ejercicios_básicos;
  * -Un constructor que inicializa el array con el nombre de los países pasados por la línea de órdenes
  * -Un método llamado sorteo que devuelve el nombre del país elegido al azar de entre todos los países
  * -Un método llamado buscar que recibe como argumento el nombre de un país y devuelve la posición en que se encuentra el país en el array o -1 si no lo encuentra
+ * 
+ * Desde el método
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class EJ1 {
     
+    public static void main(String[] args) {
+        entrada();
+    }
+    
+    public static void entrada() {
+        
+    }
 }
