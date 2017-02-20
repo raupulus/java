09a6fa6d577a025eaf6ec5b11dd70569aac73172
fryntiz <tecnico@fryntiz.es>
2017-02-20 13:19:50 +0100

    Realizado método sorteo

--- Aprendiendo/Ejercicios_Objetos_Básicos/src/Ejercicios_básicos/Pais.java
+++ Aprendiendo/Ejercicios_Objetos_Básicos/src/Ejercicios_básicos/Pais.java
@@ -3,18 +3,23 @@ package Ejercicios_básicos;
  * Clase país como complemento del ejercicio 1
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
+import java.util.Random;
 public class Pais {
-    public static String[] paises;
+    public String[] paises = new String[3];
     
-    public static void Pais(String[] input) {
+    public Pais(String[] input) {
         paises = input;
     }
     
     public void sorteo() {
+        System.out.println("El pais ganador del sorteo es:");
+        Random rd = new Random();
         
+        //Número aleatorio de los valores posibles en el array
+        System.out.println(paises[rd.nextInt(paises.length)]);
     }
     
-    public void buscar() {
+    public void buscar(String posicion) {
         
     }
 }
