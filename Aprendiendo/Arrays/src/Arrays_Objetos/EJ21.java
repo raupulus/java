bb50d472e1edb5e8815af5a4d874514a32e86797
fryntiz <tecnico@fryntiz.es>
2017-02-23 13:18:07 +0100

    Exposición del problema

--- Aprendiendo/Arrays/src/Arrays_Objetos/EJ21.java
+++ Aprendiendo/Arrays/src/Arrays_Objetos/EJ21.java
@@ -1,6 +1,16 @@
 package Arrays_Objetos;
 /**
- *
+ * Se pretende diseñar una clase llamada temperatura que tiene como atributos privados mes (entero), temperatura media(double)
+ * en grados centígrados, Fahrenheit (double) como temperatura pasada a grados Fahrenheit y como miembros públicos:
+ * 
+ * -Un constructor que recibe como argumento un mes y una temperatura en grados centígrados
+ * 
+ * Para pasar temperaturas de grados Centígrados a Fahrenheit utilizar la fórmula: F=C*1.8+32;
+ * 
+ * Desde una función principal declarar un array de objetos temperatura que guarde en cada posición las temperaturas de cada mes, y que permita:
+ * 1-Mostrar la temperatura de un mes en grados centígrados y Fahrenheit
+ * 2-Mostrar la temperatura media del año
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class EJ21 {
