474a7e62c044cf278a98e537da23e38971c2caf0
fryntiz <tecnico@fryntiz.es>
2017-02-23 20:19:41 +0100

    Planteado constructor de clase

--- Aprendiendo/Arrays/src/Arrays_Objetos/Comercial.java
+++ Aprendiendo/Arrays/src/Arrays_Objetos/Comercial.java
@@ -1,8 +1,13 @@
 package Arrays_Objetos;
 /**
  *
- * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> --> https://github.com/fryntiz
  */
 public class Comercial {
+    private static String nombre;
+    private static int[] array = new int[6];
     
+    public Comercial() {
+        
+    }
 }
