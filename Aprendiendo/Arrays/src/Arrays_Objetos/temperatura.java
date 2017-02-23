780af9537115e775db35bfba467f6a45489f3de0
fryntiz <tecnico@fryntiz.es>
2017-02-23 17:37:34 +0100

    Estructura base de la clase

--- Aprendiendo/Arrays/src/Arrays_Objetos/temperatura.java
+++ Aprendiendo/Arrays/src/Arrays_Objetos/temperatura.java
@@ -1,8 +1,11 @@
 package Arrays_Objetos;
 /**
- * 
+ * Esta clase forma parte del ejercicio 21
  * @author fryntiz
  */
 public class temperatura {
     
+    public void temperatura() { //Constructor de la clase
+        
+    }
 }
