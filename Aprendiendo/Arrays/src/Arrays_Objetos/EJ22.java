b57da5523b15da50b541a174f6b736d0f0cb8bb1
fryntiz <tecnico@fryntiz.es>
2017-02-23 19:47:50 +0100

    Descripción del problema

--- Aprendiendo/Arrays/src/Arrays_Objetos/EJ22.java
+++ Aprendiendo/Arrays/src/Arrays_Objetos/EJ22.java
@@ -1,6 +1,19 @@
 package Arrays_Objetos;
 /**
- *
+ * Una empresa desea realizar una aplicación para controlar las ventas realizadas por sus comerciales
+ * a lo largo de una semana. Se trata de diseñar para ello una clase llamada "Comercial" con los atributos privados:
+ * -nombre
+ * -Array numérico real de 6 elementos que contiene las ventas en euros realizadas en cada uno de los 6 días laborables de la semana y los métodos:
+ * -Un constructor que recibe como argumento el nombre del comercial, dimensiona el array a 6 elementos y pide por teclado las ventas de los 6 días.
+ * -Un método llamado mostrar que escribe el nombre del comercial seguido del total de ventas realizadas por el comercial en los 6 días.
+ * -Un método llamado comisiones que devuelve el importe que le corresponde al comercial por las ventas realizadas 
+ * sabiendo que las ventas realizadas en días pares llevan una comisión del 10% mientras que las ventas realizadas en días impares llevan una comisión del 15%
+ * (Se consideran pares las posiciones 0,2,4 e impares el resto)
+ * -Un método llamado mejorDia que devuelve un String con el día que mejor ventas tuvo.
+ * (Si el mejor día está en la posición 0 devolverá lunes y así hasta la 5 que será sábado)
+ * 
+ * Desde la clase Main declara un array de objetos de tipo comercial para 5 comerciales y comprueba que funcionman los métodos, de la misma forma, comprueba cual
+ * es el comercial que más vende, cual ha sido de todos los comerciales el que ha vendido más en un día y qué día.
  * @author fryntiz
  */
 public class EJ22 {
