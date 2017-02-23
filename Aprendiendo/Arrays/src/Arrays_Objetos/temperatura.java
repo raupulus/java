ebbc35492d7c074006adc1b64bcd4f0a2f3b8489
fryntiz <tecnico@fryntiz.es>
2017-02-23 19:09:38 +0100

    Clase desarrrollada

--- Aprendiendo/Arrays/src/Arrays_Objetos/temperatura.java
+++ Aprendiendo/Arrays/src/Arrays_Objetos/temperatura.java
@@ -4,11 +4,28 @@ package Arrays_Objetos;
  * @author fryntiz
  */
 public class temperatura {
-    private int mes;
+    private String mes;
+    private Double temp;
     private double tmp_media_grado, tmp_media_fahrenheit;
     
     
-    public void temperatura() { //Constructor de la clase
-        
+    public temperatura(String mesInput, Double tempInput) { //Constructor de la clase
+        mes = mesInput;
+        temp = tempInput;
     }
+    
+    public void temperatura() {
+        System.out.println("La temperatura en grados Centígrados es: " + temp + "ºC");
+        System.out.println("La temperatura en grados Fahrenheit es: " + (temp*1.8+32) + "ºF");
+    }
+    
+    public void temperaturaMedia() {
+        System.out.println("La temperatura media es");
+    }
+    
+    public Double devolverTemperatura() {
+        
+        return temp;
+    }
+    
 }
