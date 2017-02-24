521ec9564c6b5ea97a56d31ad37c50812e46e4ff
fryntiz <tecnico@fryntiz.es>
2017-02-24 16:53:57 +0100

    Planteada solución al ejercicio 23

--- Aprendiendo/Arrays/src/arrays/EJ23.java
+++ Aprendiendo/Arrays/src/arrays/EJ23.java
@@ -9,5 +9,35 @@ package arrays;
  * https://github.com/fryntiz
  */
 public class EJ23 {
+    public static int[] valores = {2,5,1,6,3,4,8,32,21,62,23,73,21,46,28,98,423,49,23,71,51,422,638};
+    public static int contadorburbuja = 0, contadorInserccion = 0, contadorSeleccion = 0, tmp;
     
+    public static void main(String[] args) {
+        burbuja();
+        inserccionDirecta();
+        seleccionDirecta();
+        System.out.println("\n\nEl método burbuja ha realizado \"" + contadorburbuja + "\" iteraciones");
+        System.out.println("\nEl método Dicotómico ha realizado \"" + contadorSeleccion + "\" iteraciones");
+        System.out.println("\nEl método Dicotómico ha realizado \"" + contadorInserccion + "\" iteraciones");
+    }
+    
+    public static void burbuja() {
+        for(int i=0;i<valores.length;i++){
+            contadorburbuja++;
+            //Método para ordenar array por Burbuja
+        }
+    }
+    
+    public static void inserccionDirecta() {
+        for(int i=0;i<valores.length;i++){
+            contadorInserccion++;
+            //Método para ordenar array por Insercción directa
+        }
+    }
+    public static void seleccionDirecta() {
+        for(int i=0;i<valores.length;i++){
+            contadorSeleccion++;
+            //Método para ordenar array por Selección directa
+        }
+    }
 }
