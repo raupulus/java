833bebaf8bb4747b80877d8102c1da6a449b458c
fryntiz <tecnico@fryntiz.es>
2017-02-24 16:43:04 +0100

    Planteado problema

--- Aprendiendo/Arrays/src/arrays/EJ23.java
+++ Aprendiendo/Arrays/src/arrays/EJ23.java
@@ -1,6 +1,10 @@
 package arrays;
 /**
- *
+ * Realiza un programa que cree un vector de 500 posiciones cargado con valores aleatorios.
+ * Los valores aleatorios deberán de estar entre el 1 y el 1000.
+ * Una vez cargado el vector deberá de ordenarlo mediante el método de la burbuja,
+ * insercción directa y selección directa y mostrarlo ordenado por pantalla.
+ * Calcula el número de iteraciones que han sido necesarias en cada método.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  * https://github.com/fryntiz
  */
