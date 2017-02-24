33e490c8851d4877febe5d0d6860ee359532532f
fryntiz <tecnico@fryntiz.es>
2017-02-24 15:37:11 +0100

    Método mostrar() y comisiones completado

--- Aprendiendo/Arrays/src/Arrays_Objetos/Comercial.java
+++ Aprendiendo/Arrays/src/Arrays_Objetos/Comercial.java
@@ -5,7 +5,7 @@ package Arrays_Objetos;
  */
 public class Comercial {
     private static String nombre;
-    private static int[] ventas = new int[6];
+    private static int[] ventas;
     private static String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"};
     
     public Comercial(String nombreInput, int longitudInput) {
@@ -22,14 +22,24 @@ public class Comercial {
     
     public void mostrar() {
         //Mostrar nombre del vendedor
+        System.out.println("Nombre del vendedor " + nombre);
+        
         //Mostrar la comisión para cada día
+        System.out.println("Comisión  para cada día:");
+        for(int i=0;i<dias.length;i++){
+            comisiones(i);
+        }
     }
     
-    public void comisiones() {
+    public void comisiones(int dia) {
         //Comisiones días pares 10% de las ventas (0,2,4)
+        if(dia%2 == 0){
+            System.out.println("El " + dias[dia] + " fue par y la comisión del 10% fue: " + ((ventas[dia]*10)/100));
+        }
         //Comisiones días impares 15% de las ventas
-        
-        //Hacer sumatorio de ventas y sacar módulo entre 2, si sale sin resto es par
+        else if(dia%2 != 0){
+            System.out.println("El " + dias[dia] + " fue impar y la comisión del 15% fue: " + ((ventas[dia]*15)/100));
+        }
     }
     
     public void mejorDia() {
