ec034ca0b4b9dfb334da2f3fdc637dd3544e1d4f
fryntiz <tecnico@fryntiz.es>
2017-02-24 18:51:16 +0100

    Problema planteado

--- Aprendiendo/Arrays/src/arrays/EJ24.java
+++ Aprendiendo/Arrays/src/arrays/EJ24.java
@@ -1,6 +1,22 @@
 package arrays;
 /**
- *
+ * Diseñar una clase llamada Curso que tiene como atributos privados el nombre
+ * del curso y como miembros públicos:
+ * -Un constructor que recibe como argumento el número de alumnos dimensiona el 
+ * array y pide los nombres por teclado.
+ * -Un método llamado elMasLargo que devuelve el nombre que tenga más caracteres
+ * -Un método llamado desplaza que desplaza todos los nombres un lugar a la derecha
+ * de forma que el último pasa a ocupar la primera posición.
+ * -Un método llamado verNombre que recibe como argumento una posición y devuelve
+ * el nombre que está en esa posición.
+ * 
+ * Desde una función principal declarar un array de 4 objetos de la clase curso (1ºDAW, 2ºDAW, 1ºSMR, 2ºSMR)
+ * 
+ * Desarrollar un menú que muestre:
+ * 1 - Resumen de un curso pedido por teclado
+ * 2 - Mostrar el alumno con el nombre más largo y en qué curso está
+ * 3 - Mostrar qué clase tiene el alumno con el nombre más corto
+ * 0 - Salir
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
