7a0d63cabfeb193c231e337dd09ac19eccef85cb
fryntiz <tecnico@fryntiz.es>
2017-02-26 23:13:39 +0100

    Descripción del problema

--- Aprendiendo/Arrays/src/arrays/EJ17.java
+++ Aprendiendo/Arrays/src/arrays/EJ17.java
@@ -1,6 +1,9 @@
 package arrays;
 /**
- *
+ * Realiza un progama que pida 10 números por teclado y que los almacene en un array.
+ * A continuación se mostrará el contenido de ese array junto al índice (0 - 9).
+ * Seguidamente el programa pasará los primos a las primeras posiciones, desplazando el resto de núm,eros (los que no son primos)
+ * de tal forma que no se pierda ninguno. Al final se debe mostrar el array resultante.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class EJ17 {
