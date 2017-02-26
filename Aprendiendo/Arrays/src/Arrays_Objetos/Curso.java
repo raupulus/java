253c02eee603583be96671325e4556d8f3896499
fryntiz <tecnico@fryntiz.es>
2017-02-26 20:25:03 +0100

    Planteados métodos de clase

--- Aprendiendo/Arrays/src/Arrays_Objetos/Curso.java
+++ Aprendiendo/Arrays/src/Arrays_Objetos/Curso.java
@@ -4,5 +4,23 @@ package Arrays_Objetos;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class Curso {
+    private String[] alumnos;
     
+    public Curso(int longitud) {
+        //Entrada longitud del array
+    }
+    
+    public void elMasLargo() {
+        //Devuelve el nombre que tenga más caracteres
+    }
+    
+    public void desplaza() {
+        //Desplaza todos los nombres
+        //Último nombre pasa a primera posición
+    }
+    
+    public String verNombre(int posicion) {
+        //Entrada de posición para devolver nombre de alumno en ella
+        return alumnos[posicion];
+    }
 }
