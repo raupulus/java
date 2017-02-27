def826b9103f2483cbebec441f7cc1622550ae9b
fryntiz <tecnico@fryntiz.es>
2017-02-27 01:30:28 +0100

    Planteada solución

--- Aprendiendo/Arrays/src/arrays/EJ17.java
+++ Aprendiendo/Arrays/src/arrays/EJ17.java
@@ -1,11 +1,23 @@
 package arrays;
 /**
- * Realiza un progama que pida 10 números por teclado y que los almacene en un array.
+ * Realiza un programa que pida 10 números por teclado y que los almacene en un array.
  * A continuación se mostrará el contenido de ese array junto al índice (0 - 9).
- * Seguidamente el programa pasará los primos a las primeras posiciones, desplazando el resto de núm,eros (los que no son primos)
- * de tal forma que no se pierda ninguno. Al final se debe mostrar el array resultante.
+ * Seguidamente el programa pasará los primos a las primeras posiciones,
+ * desplazando el resto de números (los que no son primos) de tal forma que no
+ * se pierda ninguno. Al final se debe mostrar el array resultante.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class EJ17 {
+    public static void main(String[] args) {
+        entrada();
+        ordenar();
+    }
     
+    public static void entrada() {
+        //Pide los diez números por teclado
+    }
+    
+    public static void ordenar() {
+        
+    }
 }
