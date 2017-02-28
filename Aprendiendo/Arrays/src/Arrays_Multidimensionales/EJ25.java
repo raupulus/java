d0e47a0e34628eb73c1eebf7b38de859a1498ece
fryntiz <tecnico@fryntiz.es>
2017-02-28 09:56:35 +0100

    Descripción del problema para ejercicio 25 con array multidimensional

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ25.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ25.java
@@ -1,6 +1,7 @@
 package Arrays_Multidimensionales;
 /**
- *
+ * Realizar un programa que genere un matriz de 5x8 y que solicite al usuario de
+ * manera intuitiva los valores para insertarlos en dicha matriz.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class EJ25 {
