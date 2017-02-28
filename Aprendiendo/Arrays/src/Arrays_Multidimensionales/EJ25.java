7ee8b274a251e2d5d53cca0342fc95dda5e1ad4b
fryntiz <tecnico@fryntiz.es>
2017-02-28 10:19:26 +0100

    Clase scanner importada

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ25.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ25.java
@@ -4,7 +4,11 @@ package Arrays_Multidimensionales;
  * manera intuitiva los valores para insertarlos en dicha matriz.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
+import java.util.Scanner;
 public class EJ25 {
+    //Importo la clase Scanner para entrada de datos
+    private static final Scanner sc = new Scanner(System.in);
+    
     //Creo la matriz
     private static int[][] matriz = new int[2][3];
     
