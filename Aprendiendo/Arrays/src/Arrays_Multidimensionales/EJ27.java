0dd1259b336dfa84e04b27c86c0c8d4c2967462e
fryntiz <tecnico@fryntiz.es>
2017-02-28 13:44:25 +0100

    Descripción del problema

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ27.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ27.java
@@ -1,6 +1,8 @@
 package Arrays_Multidimensionales;
 /**
- *
+ * Escribe un programa que pida 20 números enteros. Estos números se deben
+ * introducir en una matriz de 4 filas por 5 columnas. El programa mostrará las
+ * sumas parciales de filas y columnas igual que si de una hoja de cálculo se tratara.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class EJ27 {
