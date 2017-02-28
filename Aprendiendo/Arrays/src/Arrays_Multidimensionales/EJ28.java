4e3afc27f2cf77572c91343d35589587ac0958b2
fryntiz <tecnico@fryntiz.es>
2017-02-28 14:08:46 +0100

    Problema planteado para EJ 28

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ28.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ28.java
@@ -1,6 +1,7 @@
 package Arrays_Multidimensionales;
 /**
- * 
+ * Ejercicio en el que se rellene un array de dos dimensiones con números pares,
+ * lo pinte y después que pida una posición X,Y y mostrar el número correspondiente.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class EJ28 {
