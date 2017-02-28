5afb37bf817d75f987a0f462f74b08f94ba1af14
fryntiz <tecnico@fryntiz.es>
2017-02-28 13:32:51 +0100

    Problema planteado

---
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ26.java
@@ -0,0 +1,12 @@
+package Arrays_Multidimensionales;
+/**
+ * Que muestre los primeros 100 números de izquierda a derecha
+ * usando un array de dos dimensiones
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ */
+
+//No entiendo exactamente que hacer en este ejercicio
+
+public class EJ26 {
+    
+}
