f44e5812d8ed3ea2e92158787a520c42ebdcfef2
fryntiz <tecnico@fryntiz.es>
2017-02-28 14:51:11 +0100

    Estructura básica de main y método

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ29.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ29.java
@@ -7,4 +7,11 @@ package Arrays_Multidimensionales;
  */
 public class EJ29 {
     
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void NOMBRE() {
+        
+    }
 }
