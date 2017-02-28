da98ca04082e28c8ca17340eb89496fc152a1064
fryntiz <tecnico@fryntiz.es>
2017-02-28 14:50:37 +0100

    Descripción del problema para el ejercicio 29

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ29.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ29.java
@@ -1,6 +1,8 @@
 package Arrays_Multidimensionales;
 /**
- *
+ * Realiza un programa que rellene una matriz de 6 filas por 10 columnas con números
+ * enteros positivos comprendidos entre 0 y 1000 (ambos incluidos).
+ * A continuación, el programa deberá dar la posición tanto del máximo como del mínimo.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class EJ29 {
