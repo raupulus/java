a8cdf96ac40f707b7ad62bf4fdf74c7a3e84b861
fryntiz <tecnico@fryntiz.es>
2017-03-01 13:20:47 +0100

    Método para sacar el máximo y el mínimo. Ejercicio 29 concluido

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ29.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ29.java
@@ -7,7 +7,7 @@ package Arrays_Multidimensionales;
  */
 import java.util.Random;
 public class EJ29 {
-    private static int filas=6,columnas=10, minimo,minimoF,minimoC, maximo,maximoF,maximoC;
+    private static int filas=6,columnas=10, minimo=1000,minimoF,minimoC, maximo,maximoF,maximoC;
     private static int[][] matriz = new int[filas][columnas];
     static Random rd = new Random();
     
@@ -15,11 +15,8 @@ public class EJ29 {
         //Genera los números aleatorios
         generarNumeros();
         
-        //Busca el número más grande
-        maximo();
-        
-        //Busca el número más pequeño
-        minimo();
+        //Busca el número más grande y el más pequeño
+        maximoYminimo();
         
         //Muestra el resultado
         mostrar();
@@ -29,18 +26,32 @@ public class EJ29 {
         System.out.println("El array contiene " + (filas*columnas) + " números, los cuales son los siguientes:");
         for (int i=0;i<filas;i++){
             for (int j=0;j<columnas;j++) {
-                matriz[i][j] = rd.nextInt(1000);
+                matriz[i][j] = rd.nextInt(1001);
                 System.out.print(matriz[i][j] + ", ");
             }
         }
+        System.out.println("\n");
     }
     
-    public static void maximo() {
-        
-    }
-    
-    public static void minimo() {
-        
+    public static void maximoYminimo() {
+        for (int i=0;i<filas;i++){
+            for (int j=0;j<columnas;j++) {
+                
+                //Comprobar número mayor
+                if(matriz[i][j]>maximo) {
+                    maximo = matriz[i][j];
+                    maximoF = i;
+                    maximoC = j;
+                }
+                
+                //Comprobar número menor
+                if (matriz[i][j]<minimo) {
+                    minimo = matriz[i][j];
+                    minimoF = i;
+                    minimoC = j;
+                }
+            }
+        }
     }
     
     public static void mostrar() {
