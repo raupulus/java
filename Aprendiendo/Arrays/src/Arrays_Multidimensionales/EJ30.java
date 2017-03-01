c5603d92c766256b137b57707fea64be75580943
fryntiz <tecnico@fryntiz.es>
2017-03-01 13:31:48 +0100

    Descripción del problema

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ30.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ30.java
@@ -1,6 +1,7 @@
 package Arrays_Multidimensionales;
 /**
- *
+ * Que rellene una matriz de 3x3 y muestre su traspuesta (la traspuesta se
+ * consigue intercambiando filas por columnas y viceversa).
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class EJ30 {
