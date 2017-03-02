76062a1d3802f3389d881329c03469b91d056b26
fryntiz <tecnico@fryntiz.es>
2017-03-02 17:45:29 +0100

    Planteada entrada

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ27.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ27.java
@@ -5,15 +5,21 @@ package Arrays_Multidimensionales;
  * sumas parciales de filas y columnas igual que si de una hoja de cálculo se tratara.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
+import java.util.Scanner;
 public class EJ27 {
-    private static int filas=4, columnas=5;
+    private static int filas=5, columnas=6;
     private static int[][] matriz = new int[filas][columnas];
+    private static final Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args) {
         entrada();
     }
     
     public static void entrada() {
-        
+        for (int i=0;i<filas-1;i++) {
+            for (int j=0;j<columnas-1;j++) {
+                
+            }
+        }
     }
 }
