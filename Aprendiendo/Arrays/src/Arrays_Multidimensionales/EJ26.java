3ffb9f5e076dd0bf345209cb0f3e22bf0ecef969
fryntiz <tecnico@fryntiz.es>
2017-03-02 17:30:03 +0100

    Método generar aleatorios completadoos

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ26.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ26.java
@@ -5,12 +5,12 @@ package Arrays_Multidimensionales;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 
-//No entiendo exactamente que hacer en este ejercicio
-
 import java.util.Random;
 
 public class EJ26 {
     static Random rd = new Random();
+    private static int filas=30,columnas=60;
+    private static int[][] matriz = new int[filas][columnas];
     
     public static void main(String[] args) {
         //Llamada a método que genera números aleatorios
@@ -19,5 +19,10 @@ public class EJ26 {
     
     public static void generarAleatorios() {
         //x = rd.nextInt(1000); //numero máximo 1000
+        for(int i=0;i<filas;) {
+            for(int j=0;j<columnas;j++) {
+                matriz[i][j] = rd.nextInt(1001);
+            }
+        }
     }
 }
