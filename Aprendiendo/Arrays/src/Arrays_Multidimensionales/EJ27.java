d98bdfa336270e674c4241264a8c0b2297062091
fryntiz <tecnico@fryntiz.es>
2017-03-02 18:36:29 +0100

    Mejorado mostrar

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ27.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ27.java
@@ -7,29 +7,26 @@ package Arrays_Multidimensionales;
  */
 import java.util.Scanner;
 public class EJ27 {
-    private static int filas=5, columnas=6,contadorF,contadorC;
+    private static int filas=5, columnas=6,contadorF,contadorC;;
     private static int[][] matriz = new int[filas][columnas];
     private static final Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args) {
+        //Entrada port teclado de valores
         entrada();
+        //Mostrar valores
         mostrar();
     }
     
     public static void entrada() {
-        System.out.println("mierda");
+        System.out.println("");
         for (int i=0;i<filas-1;i++) {
-            System.out.println("mierda");
             for (int j=0;j<columnas-1;j++) {
-                System.out.println("mierda");
-                if(j == (columnas-1)) {
-                    System.out.println("mierda 1");
+                if(j == (columnas-2)) {
                     matriz[i][j] = contadorC;
                 } else {
-                    System.out.println("mierda2");
                     matriz[i][j] = sc.nextInt();
                     contadorC += matriz[i][j];
-                    System.out.println("mierda 3");
                 }
             }
         }
@@ -46,6 +43,12 @@ public class EJ27 {
         //Mostrar la suma de las columnas almacenadas en la última posición
         for (int i=0;i<filas-1;i++) {
             for (int j=columnas-1;j<columnas-1;j++) {
+                System.out.println("Columna " + i + " suma un total de --> " + matriz[i][filas-1]);
+            }
+        }
+        
+        for (int i=0;i<filas;i++) {
+            for (int j=columnas-1;j==columnas-1;j++) {
                 System.out.println("Columna " + i + " suma un total de --> " + matriz[i][filas-1]);
             }
         }
