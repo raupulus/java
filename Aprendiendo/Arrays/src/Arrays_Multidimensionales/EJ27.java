d9e7d99b468c9befa0f3d6ef6e34eb6a1bbc5b4b
fryntiz <tecnico@fryntiz.es>
2017-03-02 18:08:46 +0100

    Método mostrar mejorado

--- Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ27.java
+++ Aprendiendo/Arrays/src/Arrays_Multidimensionales/EJ27.java
@@ -13,22 +13,41 @@ public class EJ27 {
     
     public static void main(String[] args) {
         entrada();
+        mostrar();
     }
     
     public static void entrada() {
+        System.out.println("mierda");
         for (int i=0;i<filas-1;i++) {
+            System.out.println("mierda");
             for (int j=0;j<columnas-1;j++) {
+                System.out.println("mierda");
                 if(j == (columnas-1)) {
+                    System.out.println("mierda 1");
                     matriz[i][j] = contadorC;
                 } else {
+                    System.out.println("mierda2");
                     matriz[i][j] = sc.nextInt();
                     contadorC += matriz[i][j];
+                    System.out.println("mierda 3");
                 }
             }
         }
     }
     
     public static void mostrar() {
+        System.out.println("Array completo");
+        for (int i=0;i<filas-1;i++) {
+            for (int j=0;j<columnas-1;j++) {
+                System.out.print(matriz[i][j] + ", ");
+            }
+        }
         
+        //Mostrar la suma de las columnas almacenadas en la última posición
+        for (int i=0;i<filas-1;i++) {
+            for (int j=columnas-1;j<columnas-1;j++) {
+                System.out.println("Columna " + i + " suma un total de --> " + matriz[i][filas-1]);
+            }
+        }
     }
 }
