19032d118fa786802c787069fd6b8c79d2896268
fryntiz <tecnico@fryntiz.es>
2017-03-13 19:20:31 +0100

    main creado

--- Aprendiendo/Clase_String/src/Básicos/EJ38.java
+++ Aprendiendo/Clase_String/src/Básicos/EJ38.java
@@ -1,9 +1,13 @@
 package Básicos;
 /**
  * Leer una cadena y decir las vocales que tiene
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
 public class EJ38 {
     
+    public static void main(String[] args) {
+        
+    }
 }
