f3f03c6538ce4b5b3932321cbb14f1aeb9215f01
fryntiz <tecnico@fryntiz.es>
2017-03-13 19:25:46 +0100

    Importada libreria y clase scanner

--- Aprendiendo/Clase_String/src/Básicos/EJ38.java
+++ Aprendiendo/Clase_String/src/Básicos/EJ38.java
@@ -5,9 +5,13 @@ package Básicos;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
+import java.util.Scanner;
 public class EJ38 {
+    private static Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args) {
+        System.out.println("Introduce una cadena para contar los carácteres");
         
+        String input = new sc.next();
     }
 }
