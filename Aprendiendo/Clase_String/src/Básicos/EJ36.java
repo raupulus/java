34385e550390ccb0a7fc639d5ef3dbc124faf592
fryntiz <tecnico@fryntiz.es>
2017-03-13 18:59:09 +0100

    Problema planteado para Ej36

--- Aprendiendo/Clase_String/src/Básicos/EJ36.java
+++ Aprendiendo/Clase_String/src/Básicos/EJ36.java
@@ -1,6 +1,7 @@
 package Básicos;
 /**
- *
+ * Muestra por pantalla el resultado de la comparación de las cadenas "HOLA" y "BOLA"
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
