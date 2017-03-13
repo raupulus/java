da5b9b234ca703ecd9da863a3b1e738eacfdec6d
fryntiz <tecnico@fryntiz.es>
2017-03-13 18:48:09 +0100

    Base para EJ 35

---
+++ Aprendiendo/Clase_String/src/Básicos/EJ35.java
@@ -0,0 +1,13 @@
+package Básicos;
+/**
+ * Declara dos variables de tipos String llamadas s1 y s2 e inicialízalas
+ * con la cadena literal "HOLA". ¿La condición s1==s2 es verdadera o falsa en sete caso)
+ * ¿Y la condición s1.equals(s2)? ¿Por qué?
+ * Haz lo mismo pero creando los objetos String usando el operador new ¿Que ocurre ahora?
+ * 
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
+ * https://github.com/fryntiz
+ */
+public class EJ35 {
+
+}
