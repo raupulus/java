f2644af699ff7963564e5b1ebcae6a4ba3158077
fryntiz <tecnico@fryntiz.es>
2017-03-17 16:28:34 +0100

    Descripción del problema

--- Aprendiendo/Clase_String/src/Básicos/EJ40.java
+++ Aprendiendo/Clase_String/src/Básicos/EJ40.java
@@ -1,6 +1,9 @@
 package Básicos;
 /**
- *
+ * Crea y compara dos cadenas escogidas aleatoriamente de un array que contenga
+ * nombre de países usando compareTo(). ¿Qué ocurre cuando las cadenas son de
+ * distinta longitud?
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
