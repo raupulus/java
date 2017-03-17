616e1c266a0158700e5a8d093197740ec67cda84
fryntiz <tecnico@fryntiz.es>
2017-03-17 16:38:22 +0100

    Base para EJ

--- Aprendiendo/Clase_String/src/Básicos/EJ40.java
+++ Aprendiendo/Clase_String/src/Básicos/EJ40.java
@@ -7,6 +7,14 @@ package Básicos;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
+import java.util.Random;
 public class EJ40 {
+    private static rd = new Random();
+    private static String[] array = {"España", "Francia", "Irlanda", "Reino Unido", "Cataluña", "Andalucía"};
     
+    public static void main(String[] args) {
+        
+        
+        //x = rd.nextInt(1000); //numero máximo 1000
+    }
 }
