9b3358af3efbda845fde79a73b82326ed831bfd0
fryntiz <tecnico@fryntiz.es>
2017-03-17 17:41:30 +0100

    EJ 44 estructura de menú

--- Aprendiendo/Clase_String/src/Básicos/EJ44.java
+++ Aprendiendo/Clase_String/src/Básicos/EJ44.java
@@ -26,16 +26,37 @@ public class EJ44 {
     public static void menu() {
         boolean salir = false;
         
-        while (!salir) {
+        while (salir) {
+            System.out.println("0 - SALIR!!!");
+            System.out.println("1 - Método equals");
+            System.out.println("2 - Método compareTo");
+            System.out.println("3 - valueOf.toCharArray");
+            System.out.println("4 - length");
+            System.out.println("5 - concat(cadena2)");
+            System.out.println("6 - charAt(indice)");
+            System.out.println("7 - substring(desde,hasta)");
+            System.out.println("8 - indexOf()");
+            System.out.println("9 - lasIndexOf(),");
+            System.out.println("10 - endsWith(texto)");
+            System.out.println("11 - startsWith(texto)");
+            System.out.println("12 - replace(caracter1,caracter2)");
+            System.out.println("13 - replaceALL(cadena1,cadena2)");
+            System.out.println("14 - toUpperCase()");
+            System.out.println("15 - toLowerCase()");
+            System.out.println("16 - split(patrón");
             
-            
-            
+            input = sc.nextInt();
             
             switch (input) {
+                case 0:
+                    salir = true;
+                    break;
                 case 1:
                     
                     break;
-                case 2:
+                default:
+                    
+                    break;
             }
         }
     }
