ea0e1be5830c0edd8e51860e95eb6d752b8a7cd2
fryntiz <tecnico@fryntiz.es>
2017-03-17 16:24:47 +0100

    EJ 39 planteado a partir del EJ38

--- Aprendiendo/Clase_String/src/Básicos/EJ39.java
+++ Aprendiendo/Clase_String/src/Básicos/EJ39.java
@@ -5,9 +5,28 @@ package Básicos;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
+import java.util.Scanner;
 public class EJ39 {
-
+    private static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
+        System.out.println("Introduce una cadena para contar los carácteres");
         
+        String input = sc.nextLine(); //Pide introducir la cadena
+        
+        System.out.println("La cadena contiene " + input.length() + " caracteres"); //Muestra la cadena y su longitud
+        
+        int contador = 0;
+        String tmp;
+        
+        for (int i = 0; i< input.length(); i++) { //Comprueba que es una vocal y suma en el contador si se cumple
+            tmp = ""+input.charAt(i);
+            if (tmp.matches("[A-Z]")) {
+                contador++;
+            }
+        }
+        
+        System.out.println("\nEl número de Mayúsculas es " + contador); //Muestra las vocales
     }
 }
+
