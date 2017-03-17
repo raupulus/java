605b196a7f85330d8bc66ae7141fb7a82420aa88
fryntiz <tecnico@fryntiz.es>
2017-03-17 17:24:06 +0100

    Descripción del problema

--- Aprendiendo/Clase_String/src/Básicos/EJ44.java
+++ Aprendiendo/Clase_String/src/Básicos/EJ44.java
@@ -1,6 +1,9 @@
 package Básicos;
 /**
- *
+ * Escribe un programa en Java que pida dos cadenas y entre en un menú donde se
+ * utilicen todos los métodos vistos en clase para la manipulación de cadenas
+ * (Strings) y vayas mostrando sus resultados:
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
