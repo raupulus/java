6b0744c9c33fd61e360406c0aba4f484f2aaac12
fryntiz <tecnico@fryntiz.es>
2017-03-17 17:29:38 +0100

    Planteado ejercicio estructura

--- Aprendiendo/Clase_String/src/Básicos/EJ44.java
+++ Aprendiendo/Clase_String/src/Básicos/EJ44.java
@@ -7,6 +7,36 @@ package Básicos;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
+import java.util.Scanner;
 public class EJ44 {
+    private static final Scanner sc = new Scanner(System.in);
+    private static int input;
     
+    public static void main(String[] args) {
+        entrada();
+        menu();
+    }
+    
+    public static void entrada() {
+        //Entrada de cadenas para comenzar el programa
+        String C1 = sc.next();
+        String C2 = sc.next();
+    }
+    
+    public static void menu() {
+        boolean salir = false;
+        
+        while (!salir) {
+            
+            
+            
+            
+            switch (input) {
+                case 1:
+                    
+                    break;
+                case 2:
+            }
+        }
+    }
 }
