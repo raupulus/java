6a43fc8e8940e64d949af6eb9808a3f34327a526
fryntiz <tecnico@fryntiz.es>
2017-03-17 17:18:11 +0100

    Planteado ejercicio

--- Aprendiendo/Clase_String/src/Básicos/EJ41.java
+++ Aprendiendo/Clase_String/src/Básicos/EJ41.java
@@ -13,8 +13,17 @@ public class EJ41 {
     
     public static void main(String[] args) {
         System.out.println("Introduce 3 palabras para ordenar");
+        input[0] = sc.next();
         input[1] = sc.next();
         input[2] = sc.next();
-        input[3] = sc.next();
+        
+        String tmp;
+        for(int i = 0; i<input.length;i++) {
+            for(int j = 0; j<input.length;j++) {
+                if (input[i] > input[j]){
+                    
+                }
+            }
+        }
     }
 }
