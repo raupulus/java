4597a622f0600255883b9983fb67305b2b4f6f40
fryntiz <tecnico@fryntiz.es>
2017-03-17 16:49:42 +0100

    Problema planteado

--- Aprendiendo/Clase_String/src/Básicos/EJ41.java
+++ Aprendiendo/Clase_String/src/Básicos/EJ41.java
@@ -1,6 +1,8 @@
 package Básicos;
 /**
- *
+ * Modifica el método de ordenación por insercción directa para ordenar un
+ * array de objetos String.
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
