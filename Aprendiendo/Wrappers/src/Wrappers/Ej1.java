bc2b4ae6bfdd2ef82949673b7f62d58c174c6f3d
fryntiz <tecnico@fryntiz.es>
2017-03-23 20:11:49 +0100

    Descripción del problema para el ejercicio 1

--- Aprendiendo/Wrappers/src/Wrappers/Ej1.java
+++ Aprendiendo/Wrappers/src/Wrappers/Ej1.java
@@ -1,9 +1,11 @@
 package Wrappers;
 /**
- *
+ * Crea dos wrappers de tipo entero con el mismo valor y compáralo con los
+ * operadores de == y con el método equals
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
 public class Ej1 {
-    
+
 }
