b5e0123bd8e4f4e9648a3b8c0ba7ee61d812e128
fryntiz <tecnico@fryntiz.es>
2017-03-25 23:39:34 +0100

    Ejercicio 2 de Wrapper terminado

--- Aprendiendo/Wrappers/src/Wrappers/Ej2.java
+++ Aprendiendo/Wrappers/src/Wrappers/Ej2.java
@@ -12,27 +12,25 @@ public class Ej2 {
     private static Integer input,inputInvertido;
 
     public static void main(String[] args) {
-        entrada();
-        comprobar();
-        if (input == inputInvertido) {
+        entrada(); //Pedir número por teclado
+        comprobar(); //Comprobar que es capicúa
+        
+        if (input == inputInvertido) { //Compruebo que el número de entrada sea igual al mismo invertido
             System.out.println("Es capicúa (invertido es --> " + inputInvertido + ")");
         } else {
             System.out.println("No es capicúa (invertido es --> " + inputInvertido + ")");
         }
     }
     
-    public static void entrada() {
+    public static void entrada() { //Pedir número por teclado
         System.out.println("Introduce el número a comprobar si es capicúa:");
         input = sc.nextInt();
     }
     
-    public static void comprobar() {
-        Integer tmp;
-        String alreves = "";
-        tmp = input;
-        while (tmp > 0) {
-            alreves = alreves.concat(Integer.toString(tmp%10)); //Cadena donde guardo el número invertido
-            tmp = tmp/10; //Quito la parte del resto añadido a la cadena "alrves"
-        }
+    public static void comprobar() { //Comprobar que es capicúa
+        StringBuilder alreves = new StringBuilder(Integer.toString(input)); //Convierto a cadena el número de entrada
+        alreves = alreves.reverse(); //Invertir carácteres de la cadena
+        
+        inputInvertido = Integer.valueOf(alreves.toString()); //Devuelvo a int el número invertido
     }
 }
