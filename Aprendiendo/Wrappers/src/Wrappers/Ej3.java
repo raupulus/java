b4a03c6b1a319b87ebe451fecfbc37e62c557b35
fryntiz <tecnico@fryntiz.es>
2017-03-25 23:46:10 +0100

    Estructura base (main y método vacios)

--- Aprendiendo/Wrappers/src/Wrappers/Ej3.java
+++ Aprendiendo/Wrappers/src/Wrappers/Ej3.java
@@ -13,6 +13,14 @@ package Wrappers;
  * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
+
 public class Ej3 {
     
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void NOMBRE() {
+        
+    }
 }
