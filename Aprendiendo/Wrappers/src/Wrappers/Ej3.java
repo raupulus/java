c4dce3be969f6220a8791f8dd4755450c868241f
fryntiz <tecnico@fryntiz.es>
2017-03-25 23:45:24 +0100

    Descripción del problema para el ejercicio 3

--- Aprendiendo/Wrappers/src/Wrappers/Ej3.java
+++ Aprendiendo/Wrappers/src/Wrappers/Ej3.java
@@ -1,5 +1,15 @@
 package Wrappers;
 /**
+ * Realiza una clase conversor que tenga las siguientes características:
+ * -Toma como parámetro en el constructor un valor entero
+ * -Tiene como parámetro en el constructor un valor entero
+ * -Tiene un método "getNumero" que dependiendo del parámetro devolverá el mismo número en el siguiente formato:
+ * 
+ * B --> Binario (String)
+ * H --> Hexadecimal (String)
+ * O --> Octal (String)
+ * 
+ * Realizar un método main en la clase para probar todo lo anterior
  * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
