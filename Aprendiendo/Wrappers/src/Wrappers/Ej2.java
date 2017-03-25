5a5609befa4d7e8369b02f8a78275f413a68fb4a
fryntiz <tecnico@fryntiz.es>
2017-03-25 20:33:57 +0100

    Descripción del problema

--- Aprendiendo/Wrappers/src/Wrappers/Ej2.java
+++ Aprendiendo/Wrappers/src/Wrappers/Ej2.java
@@ -1,6 +1,8 @@
 package Wrappers;
 /**
- *
+ * Realiza un método "esCapicua que tome como parámetro un entero y devuelva
+ * true si el número es capicúa y false en caso contrario. Utiliza
+ * wrappers y objetos String.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class Ej2 {
