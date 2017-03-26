66b772dd9c8a70e1416f8d0ca0732908d384a125
fryntiz <tecnico@fryntiz.es>
2017-03-26 03:06:24 +0200

    Funcionamiento de la clase creado

--- Aprendiendo/Wrappers/src/Wrappers/Ej3_Conversor.java
+++ Aprendiendo/Wrappers/src/Wrappers/Ej3_Conversor.java
@@ -4,8 +4,30 @@ package Wrappers;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class Ej3_Conversor {
+    public static int valorEntrada;
     
-    public static void Ej3_Conversor() {
+    public Ej3_Conversor(int entrada) {
+        valorEntrada = entrada;
+    }
+    
+    public String getNumero(String x) {
+        x = String.toUpperCase(x);
+        int binario,hexadecimal,octal;
         
+        switch (x) {
+            case "B":
+            
+            break;
+            case "H":
+            
+            break;
+            case "O":
+            
+            break;
+        }
+        
+        System.out.println("Soy el número devuelto!!!");
+        
+        return "El resultado es " + x;
     }
 }
