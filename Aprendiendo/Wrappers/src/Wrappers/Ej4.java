568b4d3c3c57cee1e622a6378ff9a774c4736ef5
fryntiz <tecnico@fryntiz.es>
2017-03-26 16:39:36 +0200

    Problema para ejercicio 4 planteado

--- Aprendiendo/Wrappers/src/Wrappers/Ej4.java
+++ Aprendiendo/Wrappers/src/Wrappers/Ej4.java
@@ -1,6 +1,12 @@
 package Wrappers;
 /**
- *
+ * Programa que transforme un número entero, mayor o igual que 0
+ * a binario de dos formas diferentes:
+ * -Utilizando algún método de la clase Integer
+ * -Creando un método que utilice arrays para almacenar los restos y el último cociente
+ * 
+ * Utilizando Wrappers, crea un programa que pida un número por teclado al usuario
+ * en binario y lo transforme a decimal, a hexadecimal y a octal.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class Ej4 {
