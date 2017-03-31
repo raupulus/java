8e6432f3e0e4a341157964e61dd30fa8f9d56f6c
fryntiz <tecnico@fryntiz.es>
2017-03-31 17:55:29 +0200

    Ejercicio 7 terminado

--- Aprendiendo/Listas/src/Ejercicios/Ej7.java
+++ Aprendiendo/Listas/src/Ejercicios/Ej7.java
@@ -10,14 +10,16 @@ import java.util.ArrayList;
 
 public class Ej7 {
     private static final Scanner sc = new Scanner(System.in);
-    private static int suma,media;
+    private static int suma=0;
+    private static double media=0;
     private static ArrayList<Integer> m = new ArrayList();
     
     public static void main(String[] args) {
         entrada();
         suma();
         media();
-        System.out.println(m.toString());
+        System.out.println("\n\n" + m.toString());
+        System.out.println("\n\nLongitud del ArrayList --> " + m.size());
         System.out.println("\n\nLa suma es --> " + suma);
         System.out.println("\n\nLa media es --> " + media);
     }
@@ -39,10 +41,13 @@ public class Ej7 {
     }
     
     public static void suma() {
-        
+        for (int i = 0; i < m.size();i++) {
+            suma += m.get(i);
+        }
     }
     
     public static void media() {
-        
+        double tmp1 = suma, tmp2 = m.size();
+        media = (tmp1/tmp2);
     }
 }
