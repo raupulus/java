c821eb04b8e7464b68b2916b5be1b3373761608d
fryntiz <tecnico@fryntiz.es>
2017-03-31 16:52:00 +0200

    Descripción del problema para ejercicio 6

--- Aprendiendo/Listas/src/Ejercicios/Ej6.java
+++ Aprendiendo/Listas/src/Ejercicios/Ej6.java
@@ -1,6 +1,32 @@
 package Ejercicios;
 /**
- *
+ * Sin usar las clases proporcionadas por JCF, crea manualmente una lista
+ * simplemente enlzada (ListaSE) de números enteros que permita las siguientes
+ * operaciones:
+ * 
+ * -voidagregar(nuevo) Añade un nuevo nodo al final de la lista. Este método
+ * estará sobrecargado para recibir un número entero o bien un nodo.
+ * 
+ * -void insertar(nuevo) Insertar un nodo al principio de lista
+ * 
+ * -booleaninsertarAntes(nuevo,nodo): Inserta el nuevo nodo antes del nodo que recibe
+ * 
+ * -booleaninsertarDespues(nuevo,nodo): Inserta el nuevo nodo después del nodo que recibe
+ * 
+ * -boolean buscar(nodo): Buscar un nodo en la lista(equals)
+ * 
+ * -int contar(nodo): Devuelve cuantos nodos hay en la lista iguales albuscado (equals)
+ * 
+ * -boolean borrar(nodo): Borra el nodo(equals)
+ * 
+ * -void borrar(): Borra todos los nodos de la lista
+ * 
+ * -listaSE filtrar(nodo): Devuelve una nueva lista que contiene todos los
+ * nodos de la lista original iguales al nodo recibido
+ * 
+ * intget(posicion) Devuelve el elemento que está en la posición indicada en la
+ * lista (primera posición = 0)
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
