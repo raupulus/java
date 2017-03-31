4dbce9549765c386a864329d2fb8d462fa473b2e
fryntiz <tecnico@fryntiz.es>
2017-03-31 17:00:20 +0200

    Descripción del problema para ej 7

--- Aprendiendo/Listas/src/Ejercicios/Ej7.java
+++ Aprendiendo/Listas/src/Ejercicios/Ej7.java
@@ -1,6 +1,7 @@
 package Ejercicios;
 /**
- *
+ * Escribe un programa que lea números enteros y los guarde en un ArrayList
+ * hasta que se lea un 0 y muestra los números leídos, su suma y su media.
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
