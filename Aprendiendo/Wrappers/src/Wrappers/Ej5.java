bfda178029010c5dc04078636c7ebe03be5aa5b4
fryntiz <tecnico@fryntiz.es>
2017-04-11 19:22:59 +0200

    Descripción del problema para EJ5

--- Aprendiendo/Wrappers/src/Wrappers/Ej5.java
+++ Aprendiendo/Wrappers/src/Wrappers/Ej5.java
@@ -1,7 +1,7 @@
 package Wrappers;
-
 /**
- *
+ * Realiza un programa que realice todas las conversiones posibles entre wrappers,
+ * String y primitivo para los Float
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class Ej5 {
