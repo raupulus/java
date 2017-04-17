a9368164b776b7fce9c640eab9af03d2ae54e6f2
fryntiz <tecnico@fryntiz.es>
2017-04-17 19:04:31 +0200

    Objetivo de la clase complementaria al ejercicio 9

--- Aprendiendo/Listas/src/Ejercicios/Atleta.java
+++ Aprendiendo/Listas/src/Ejercicios/Atleta.java
@@ -1,6 +1,13 @@
 package Ejercicios;
 /**
- *
+ * CLASE COMPLEMENTARIA DEL EJERCICIO 9
+ * 
+ * Clase Atleta con los atributos:
+ * dorsal(int),nombre(String),tiempos(int [3])
+ * Y los métodos:
+ * -Un constructor que pide los datos por teclado
+ * -Métodos get y set que creas necesarios
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
