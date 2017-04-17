7cdc1e88c8db7ff5ba66fb4a552a9362be01a127
fryntiz <tecnico@fryntiz.es>
2017-04-17 18:00:01 +0200

    Problema para ejercicio 9

--- Aprendiendo/Listas/src/Ejercicios/EJ9.java
+++ Aprendiendo/Listas/src/Ejercicios/EJ9.java
@@ -1,9 +1,30 @@
 package Ejercicios;
 /**
+ * Se pretende hacer una selección de los atletas que van a participar en una
+ * prueba de Triatlón. Diseñar para ello una clase llamada Atleta con los atributos:
+ * dorsal(int),nombre(String),tiempos(int [3])
+ * Y los métodos:
+ * -Un constructor que pide los datos por teclado
+ * -Métodos get y set que creas necesarios
+ * 
+ * Una clase carrera con los atributos:
+ * -arrayList de atletas
+ * Con los métodos:
+ * -Un constructor que crea el ArrayList
+ * 
+ * Método necesarios para llevar a cabo las tareas que se piden en el menú
+ * repetitivo siguiente:
+ * 1 - Añadir un atleta
+ * 2 - Listado de atletas con el tiempo total empleado en las 3 pruebas
+ * 3 - Borrar todos los atletas cuyo tiempo sea inferior al tiempo medio de todos
+ * 4 - Listado de los atletas seleccionados ordenado por tiempo y prueba
+ * 5 - Listado de ganadores (Sumando todos los tiempos)
+ * 6 - Buscar un Atleta (Por nombre o dorsal)
  * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
+
 public class EJ9 {
     
 }
