410ea668a2fb4a84cd62daea9be61ec9b925f898
fryntiz <tecnico@fryntiz.es>
2017-04-17 10:25:41 +0200

    Descripción del problema EJ8

--- Aprendiendo/Listas/src/Ejercicios/EJ8.java
+++ Aprendiendo/Listas/src/Ejercicios/EJ8.java
@@ -1,6 +1,20 @@
 package Ejercicios;
 /**
- *
+ * Crear un programa que permita gestionar una agenda utilizando iteradores y
+ * las siguientes clases:
+ * 
+ * -añadePersona --> Recibe como argumento un objeto de la clase "Persona" y lo
+ * añade a la lista, comprobando previamente que no exista otra persona con ese
+ * dni
+ * 
+ * -borraPersona --> Recibe como argumento un dni y si existe una persona con
+ * ese dni en la lista la elimina, devolviendo true en caso de eliminación o
+ * false en caso contrario
+ * 
+ * -buscaPersona --> Recibe como argumento el dni y comprueba si existe en la
+ * lista, si existe visualiza el nombre y devuelve true. Si no existe devuelve false
+ * 
+ * -muestraListaPersona --> Visualiza todos los elementos de la lista
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class EJ8 {
