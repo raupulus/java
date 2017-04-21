abc1b8bfa755eaddf6344e51c096eb9d19b2a411
fryntiz <tecnico@fryntiz.es>
2017-04-21 15:49:58 +0200

    Descripción del ejercicio 19

--- Aprendiendo/Calendarios_y_Fechas/src/Calendarios_y_fechas/EJ19.java
+++ Aprendiendo/Calendarios_y_Fechas/src/Calendarios_y_fechas/EJ19.java
@@ -1,6 +1,8 @@
 package Calendarios_y_fechas;
 /**
- *
+ * Practica con GregorianCalendary y SimpleDateFormat. Copia y pega un método main
+ * las siguientes sentencias.
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
