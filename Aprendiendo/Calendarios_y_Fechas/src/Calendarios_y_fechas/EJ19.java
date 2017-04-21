9c6a6acfd98e0ae196f72f8d4299494474cd0e8b
fryntiz <tecnico@fryntiz.es>
2017-04-21 16:02:15 +0200

    Ejercicio copiado

--- Aprendiendo/Calendarios_y_Fechas/src/Calendarios_y_fechas/EJ19.java
+++ Aprendiendo/Calendarios_y_Fechas/src/Calendarios_y_fechas/EJ19.java
@@ -1,4 +1,7 @@
 package Calendarios_y_fechas;
+
+import java.util.Calendar;
+
 /**
  * Practica con GregorianCalendary y SimpleDateFormat. Copia y pega un método main
  * las siguientes sentencias.
@@ -6,8 +9,31 @@ package Calendarios_y_fechas;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
+
+
+//En el ejercicio pone copiar dentro de main las sentencias pero no funciona
+//Tampoco hay documento explicando como funciona
 public class EJ19 {
     public static void main(String[] args) {
+        Calendar c1 = GregorianCalendar.getInstance();
+        System.out.println("Fecha actual:"+c1.getTime().toLocaleString());
+
+        c1.set(2000,Calendar.AUGUST,31);
+        System.out.println("Fecha 31 Agosto 2000:"+c.1getTime().toLocaleString());
+
+        c1.set(Calendar.MONTH,13);
+        System.out.println("Fecha 13 meses más:"+c1.getTime().toLocaleString);
+
+        /*Añadir 13 meses*/
+        c1.set(2000,Calendar,AUGUST,31);
+        c1.add(Calendar.MONTH,13);
+        System.out.println("Fecha 13 meses más "+c1.getTime().toLocaleString()); (Correcto)
+             
+        /*Uso de roll*/
+        c1.roll(Calendar.HOUR,25);/*Añadir 25 horas*/
+        System.out.println("Fecha 25 horas más: "+c1.getTime().toLocaleString());
+        SimpleDateFormatsdf=new SimpleDateFormat("dd/MMMMM/yyyyhh:mm:ss");
         
+        System.out.println("Fecha Formateada:"+sdf.format(c1.getTime()));
     } 
 }
