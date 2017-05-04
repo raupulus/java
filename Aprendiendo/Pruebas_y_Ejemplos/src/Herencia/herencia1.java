80a7b89c6da98d2bda4e2e87e75495868d951835
fryntiz <tecnico@fryntiz.es>
2017-05-04 22:29:32 +0200

    Pruebas de Herencia

--- Aprendiendo/Pruebas_y_Ejemplos/src/Herencia/herencia1.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Herencia/herencia1.java
@@ -1,9 +1,36 @@
 package Herencia;
+
 /**
- *
+ * Prueba de herencia
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
 public class herencia1 {
-    
+
+    public class Articulo {
+
+        private String titulo;
+        private int duracion;
+        private boolean loTengo;
+        private String comentario;
+
+        public Articulo(String titulo, int duracion) {
+            this.titulo = titulo;
+            this.duracion = duracion;
+            loTengo = false;
+            comentario = "";
+        }
+    }
+
+    public class CD extends Articulo {
+
+        private String artista;
+        private int numeroPistas;
+
+        public CD(String titulo, int duracion, String artista, int pistas) {
+            super(titulo, duracion);
+            this.artista = artista;
+            numeroPistas = pistas;
+        }
+    }
 }
