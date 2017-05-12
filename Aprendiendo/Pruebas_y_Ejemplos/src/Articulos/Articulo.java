1e5d6d289ae397307d30c7017b0abd3ae3e87419
fryntiz <tecnico@fryntiz.es>
2017-05-12 12:21:51 +0200

    Clase artículo creada

--- Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Articulo.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Articulo.java
@@ -5,6 +5,16 @@ package Articulos;
  * https://github.com/fryntiz
  */
 public class Articulo {
-    protected String titulo;
-    
+    private String titulo;
+    private int duracion;
+    private boolean loTengo;
+    private String comentario;
+
+    public Articulo(String titulo, int duracion) {
+         this.titulo = titulo;
+         this.duracion = duracion;
+         loTengo =  false;
+         comentario = "";
+    }
 }
+
