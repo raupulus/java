f6b3370c3611625b540aa66e2adbae53b784fb94
fryntiz <tecnico@fryntiz.es>
2017-05-12 13:19:48 +0200

    Comentarios de estructura

--- Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/CD.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/CD.java
@@ -1,7 +1,9 @@
 package Articulos;
 
 /**
- *
+ * Clase CD que hereda de la clase artículos, permite crear instancias de CD
+ * en la clase "Principal"
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
@@ -10,8 +12,10 @@ public class CD extends Articulo {
     private String artista;
     private int numeroPistas;
 
+    //Constructor para crear CD pasándo título, duración, artista y número de pistas
+    //Título y duración invocarán a la clase super (padre) "Articulo"
     public CD(String titulo, int duracion, String artista, int pistas) {
-        super(titulo, duracion);
+        super(titulo, duracion); //Pasa atributos al constructor
         this.artista = artista;
         numeroPistas = pistas;
     }
