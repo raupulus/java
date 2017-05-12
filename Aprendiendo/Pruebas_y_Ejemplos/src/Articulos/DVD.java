3d4718468e2dfa9cc6cc122ed076e0b85eee1452
fryntiz <tecnico@fryntiz.es>
2017-05-12 15:36:29 +0200

    Reparado error de clase

--- Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/DVD.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/DVD.java
@@ -1,12 +1,13 @@
 package Articulos;
+
 /**
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
 public class DVD extends Articulo {
-    
-    public class DVD(String titulo, int duracion) {
+
+    public DVD(String titulo, int duracion) {
         super(titulo, duracion); //Pasa atributos al constructor
     }
 }
