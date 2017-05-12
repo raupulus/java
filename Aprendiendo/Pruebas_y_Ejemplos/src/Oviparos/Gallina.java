de8777083bb55d43d2ef09bf8dd03623fa013dfb
fryntiz <tecnico@fryntiz.es>
2017-05-12 16:05:55 +0200

    Clase Gallina creada

---
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Oviparos/Gallina.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Oviparos;
+
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
+ * https://github.com/fryntiz
+ */
+public class Gallina {
+    
+}
