521ff82ef3806f1c4fd17df942ff0a0a3f3738e9
fryntiz <tecnico@fryntiz.es>
2017-05-12 12:30:43 +0200

    Reformateado

--- Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Articulo.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Articulo.java
@@ -1,13 +1,15 @@
 package Articulos;
+
 /**
- * Clase artículo que contiene la parte que será común para el resto de artículos
- * los cuales heredarán de esta clase
- * 
+ * Clase artículo que contiene la parte que será común para el resto de
+ * artículos los cuales heredarán de esta clase
+ *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
 
 public class Articulo {
+
     private String titulo;
     private int duracion;
     private boolean loTengo;
@@ -15,10 +17,9 @@ public class Articulo {
 
     //Constructor de la clase, se invoca con una cadena como título y un int duración
     public Articulo(String titulo, int duracion) {
-         this.titulo = titulo; //Asigna el título pasado al atributo de clase
-         this.duracion = duracion; //Asigna la duración pasada al atributo de clase
-         loTengo =  false; //Por defecto "false"
-         comentario = ""; //Por defecto sin comentario
+        this.titulo = titulo; //Asigna el título pasado al atributo de clase
+        this.duracion = duracion; //Asigna la duración pasada al atributo de clase
+        loTengo = false; //Por defecto "false"
+        comentario = ""; //Por defecto sin comentario
     }
 }
-
