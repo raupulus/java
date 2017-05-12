5fb6747e1b352f3fd0d45432223070285fcee643
fryntiz <tecnico@fryntiz.es>
2017-05-12 12:29:24 +0200

    Base para clase CD heredando de clase artículos

--- Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/CD.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/CD.java
@@ -4,6 +4,15 @@ package Articulos;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
-public class CD {
-    
+public class CD extends Articulo
+{
+   private String artista;
+   private int numeroPistas;
+  
+	   public CD(String titulo, int duracion, String artista, int pistas )
+	   {
+		super(titulo, duracion);
+		this.artista = artista;
+		numeroPistas = pistas;
+	    }
 }
