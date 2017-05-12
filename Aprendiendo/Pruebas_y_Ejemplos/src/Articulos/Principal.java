7b9dfeaaf7c4d8033c3ca02482e2b1150cabca23
fryntiz <tecnico@fryntiz.es>
2017-05-12 12:13:12 +0200

    Estructura para la clase principal donde asigno funciones principales

--- Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Principal.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Principal.java
@@ -5,5 +5,31 @@ package Articulos;
  * https://github.com/fryntiz
  */
 public class Principal {
-    
+    private ArrayList<CD> cds;
+    private ArrayList<DVD> dvds;
+
+    public BaseDatos() {
+            cds = new ArrayList<CD>();
+            dvds = new ArrayList<DVD>();
+    }
+
+    public void addCD(CD elCD) {
+            cds.add(elCD);
+    }
+
+    public void addDVD(DVD elDVD) {
+            dvds.add(elDVD);
+    }
+
+    public void listar() {
+        for (CD cd : cds) {
+            cd.print();
+            System.out.println();   
+        }
+
+        for (DVD dvd : dvds) {
+                dvd.print();
+                System.out.println();  
+        }
+    }
 }
