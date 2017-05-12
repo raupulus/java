64aae8ce2bf2dae98971365fb31c47fc4e6f657d
fryntiz <tecnico@fryntiz.es>
2017-05-12 17:42:05 +0200

    Creados ejemplos

--- Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Principal.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Principal.java
@@ -12,6 +12,13 @@ public class Principal {
     private ArrayList<CD> cds = new ArrayList<CD>();
     private ArrayList<DVD> dvds = new ArrayList<DVD>();
 
+    public void main(String[] args) {
+        cds.add(new CD("Los mejores éxitos", 60, "Los Chichos", 12));
+        cds.add(new CD("Arreando mulas", 43, "Los Chichos", 9));
+        dvds.add(new DVD("Blanca la enana y las 7 nieves", 121, "Troglopoco"));
+        dvds.add(new DVD("Los bollos molan más que los repollos", 154, "Falete"));
+    }
+    
     public void addCD(CD nombreCD) {
         cds.add(nombreCD);
     }
