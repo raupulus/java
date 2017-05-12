19d33ea5bfdb06814dc0dc25edaa0b54c751c8fd
fryntiz <tecnico@fryntiz.es>
2017-05-12 14:10:04 +0200

    Base para DVD

--- Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/DVD.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/DVD.java
@@ -4,6 +4,9 @@ package Articulos;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
-public class DVD {
+public class DVD extends Articulo {
     
+    public class DVD(String titulo, int duracion) {
+        super(titulo, duracion); //Pasa atributos al constructor
+    }
 }
