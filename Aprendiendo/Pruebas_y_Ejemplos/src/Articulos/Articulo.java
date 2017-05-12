f30fb2218e49dc4cbba39f3759bfc8af18bcb935
fryntiz <tecnico@fryntiz.es>
2017-05-12 12:28:53 +0200

    Comentarios sobre funcionamiento de la clase

--- Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Articulo.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Articulo.java
@@ -1,20 +1,24 @@
 package Articulos;
 /**
- *
+ * Clase artículo que contiene la parte que será común para el resto de artículos
+ * los cuales heredarán de esta clase
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
+
 public class Articulo {
     private String titulo;
     private int duracion;
     private boolean loTengo;
     private String comentario;
 
+    //Constructor de la clase, se invoca con una cadena como título y un int duración
     public Articulo(String titulo, int duracion) {
-         this.titulo = titulo;
-         this.duracion = duracion;
-         loTengo =  false;
-         comentario = "";
+         this.titulo = titulo; //Asigna el título pasado al atributo de clase
+         this.duracion = duracion; //Asigna la duración pasada al atributo de clase
+         loTengo =  false; //Por defecto "false"
+         comentario = ""; //Por defecto sin comentario
     }
 }
 
