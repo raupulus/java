d3b62ae990e7dca077a6a890d10e70bc15ef174a
fryntiz <tecnico@fryntiz.es>
2017-05-12 19:09:37 +0200

    añadida descripción del funcionamiento

--- Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Principal.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/Principal.java
@@ -3,27 +3,28 @@ package Articulos;
 import java.util.ArrayList;
 
 /**
- *
+ * Clase principal que controla la funcionalidad del programa
+ * En este ejemplo se utilizan las clases CD y DVD que heredan de Articulo
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
 public class Principal {
 
-    private static ArrayList<CD> cds = new ArrayList<CD>();
-    private static ArrayList<DVD> dvds = new ArrayList<DVD>();
+    private static ArrayList<CD> cds = new ArrayList();
+    private static ArrayList<DVD> dvds = new ArrayList();
 
     public static void main(String[] args) {
         addCD("Los mejores éxitos", 60, "Los Chichos", 12);
         addCD("Arreando mulas", 43, "Los Chichos", 9);
         addDVD("Blanca la enana y las 7 nieves", 121, "Troglopoco");
         addDVD("Los bollos molan más que los repollos", 154, "Falete");
-        
+
         listar();
     }
-    
+
     public static void addCD(String titulo, int duracion, String artista, int pistas) {
         cds.add(new CD(titulo, duracion, artista, pistas));
-        System.out.println(titulo);
     }
 
     public static void addDVD(String titulo, int duracion, String director) {
@@ -33,12 +34,10 @@ public class Principal {
     public static void listar() {
         for (CD cd : cds) {
             cd.print();
-            System.out.println();
         }
 
         for (DVD dvd : dvds) {
             dvd.print();
-            System.out.println();
         }
     }
 }
