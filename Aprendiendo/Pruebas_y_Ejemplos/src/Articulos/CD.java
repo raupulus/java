70337ba3cf753a3f79599199c1101c1692cc4d57
fryntiz <tecnico@fryntiz.es>
2017-05-12 12:30:59 +0200

    Reformateado

--- Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/CD.java
+++ Aprendiendo/Pruebas_y_Ejemplos/src/Articulos/CD.java
@@ -1,18 +1,18 @@
 package Articulos;
+
 /**
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
-public class CD extends Articulo
-{
-   private String artista;
-   private int numeroPistas;
-  
-	   public CD(String titulo, int duracion, String artista, int pistas )
-	   {
-		super(titulo, duracion);
-		this.artista = artista;
-		numeroPistas = pistas;
-	    }
+public class CD extends Articulo {
+
+    private String artista;
+    private int numeroPistas;
+
+    public CD(String titulo, int duracion, String artista, int pistas) {
+        super(titulo, duracion);
+        this.artista = artista;
+        numeroPistas = pistas;
+    }
 }
