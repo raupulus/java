320db8c1485312ecb00a6d74ee6ff4298a2d3d3d
fryntiz <tecnico@fryntiz.es>
2017-05-20 17:22:14 +0200

    Importo clases necesarias para el funcionamiento de swing

--- Aprendiendo/GUI/src/Ventanas/NombreEdad.java
+++ Aprendiendo/GUI/src/Ventanas/NombreEdad.java
@@ -4,3 +4,22 @@ package Ventanas;
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
+import java.awt.Dimension;
+
+import java.awt.FlowLayout;
+
+import java.awt.GridLayout;
+
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import javax.swing.JFrame;
+
+import javax.swing.JLabel;
+
+import javax.swing.JOptionPane;
+
+import javax.swing.JTextField;
