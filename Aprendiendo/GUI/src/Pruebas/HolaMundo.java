58b64404ecac9f473c346ad058e4c05dc8993125
fryntiz <tecnico@fryntiz.es>
2017-05-20 16:45:13 +0200

    Importada base

--- Aprendiendo/GUI/src/Pruebas/HolaMundo.java
+++ Aprendiendo/GUI/src/Pruebas/HolaMundo.java
@@ -4,6 +4,10 @@ package Pruebas;
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class HolaMundo {
-    
+
 }
