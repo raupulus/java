9d3e0c82af3add2aee832b95f089ce41ae65ac3f
fryntiz <tecnico@fryntiz.es>
2017-05-20 23:12:33 +0200

    Prueba 3 planteada

--- Aprendiendo/GUI/src/Pruebas/Prueba3.java
+++ Aprendiendo/GUI/src/Pruebas/Prueba3.java
@@ -2,16 +2,38 @@ package Pruebas;
 
 /**
  * Una clase sencilla
+ *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 import javax.swing.*;
+import java.awt.*;
 
 public class Prueba3 extends JFrame {
 
-    public static void main(String argv[]) {
-        Prueba3 ventana = new Prueba3();
-        Prueba3.getContentPane().add();
-        Prueba3.setSize(400, 400);
-        Prueba3.setVisible(true);
+    JButton boton;
+    JLabel etiqueta;
+    JButton otroBoton;
+    JPanel panel;
+
+    public static void main(String[] args) {
+        new Prueba3();
+    }
+    
+    public Prueba3() {
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        getContentPane().add(panel);
+        etiqueta = new JLabel("Una etiqueta");
+        boton = new JButton("Un botón");
+        otroBoton = new JButton("Otro Botón");
+        panel.add(boton,
+          BorderLayout.SOUTH
+        );
+        panel.add(etiqueta,
+          BorderLayout.WEST
+        );
+        panel.add(otroBoton,
+          BorderLayout.NORTH
+        );
     }
 }
