9dd3f7bae5f5ce0aaff7ade62af7a300f12762ff
fryntiz <tecnico@fryntiz.es>
2017-05-20 23:00:06 +0200

    Base para prueba sencilla

--- Aprendiendo/GUI/src/Pruebas/Prueba3.java
+++ Aprendiendo/GUI/src/Pruebas/Prueba3.java
@@ -1,9 +1,17 @@
 package Pruebas;
 
 /**
- *
+ * Una clase sencilla
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
-public class Prueba3 {
-    
+import javax.swing.*;
+
+public class Prueba3 extends JFrame {
+
+    public static void main(String argv[]) {
+        Prueba3 ventana = new Prueba3();
+        Prueba3.getContentPane().add();
+        Prueba3.setSize(400, 400);
+        Prueba3.setVisible(true);
+    }
 }
