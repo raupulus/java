e094162e003367b9b21a5a25b0ffd40e12e02900
fryntiz <tecnico@fryntiz.es>
2017-05-20 21:45:06 +0200

    Base para metodos utiles

---
+++ Aprendiendo/GUI/src/Utiles/Metodos.java
@@ -0,0 +1,22 @@
+package Utiles;
+
+/**
+ * Hoja con métodos útiles
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ */
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+
+public class Metodos extends JFrame {
+
+    public Metodos() {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
