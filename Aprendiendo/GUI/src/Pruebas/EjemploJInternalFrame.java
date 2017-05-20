48f5061e921609f5473d29c1fa3b6e074037f55c
fryntiz <tecnico@fryntiz.es>
2017-05-20 15:20:14 +0200

    Creada clase EjemploJInternalFrame

--- Aprendiendo/GUI/src/Pruebas/EjemploJInternalFrame.java
+++ Aprendiendo/GUI/src/Pruebas/EjemploJInternalFrame.java
@@ -11,6 +11,31 @@ import javax.swing.JButton;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 
-public class EjemploJInternalFrame {
+public class EjemploJInternalFrame extends JInternalFrame {
 
+    public EjemploJInternalFrame() {
+        setBounds(100, 100, 245, 134);
+        // coordenadas con las dimensiones de la ventana
+        // disposición de capas a nulo para poder posicionar
+        // con coordenadas en vez de con el esquema determinado
+        getContentPane().setLayout(null);
+
+        // Creo una etiqueta
+        JLabel lblstoEsUna = new JLabel("Esto es una ventana interna...");
+        lblstoEsUna.setBounds(10, 11, 333, 14);
+        getContentPane().add(lblstoEsUna);
+
+        // El botón que cierra ésta ventana interna
+        JButton btnAprietaAquPara = new JButton("Aprieta aquí para cerrar");
+        btnAprietaAquPara.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //Ésto se ejecuta cuando se aprieta el botón (manda clase a basura)
+                dispose(); //Cierra la ventana
+            }
+        });
+
+        btnAprietaAquPara.setBounds(10, 36, 209, 58);
+        getContentPane().add(btnAprietaAquPara);
+
+    }
 }
