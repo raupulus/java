0d9234b5ab78c72b29e1543157b1be55ab10adbe
fryntiz <tecnico@fryntiz.es>
2017-05-20 23:21:24 +0200

    Importo librerias que usaré

---
+++ Aprendiendo/GUI/src/Ventanas/BasicBorderLayout.java
@@ -0,0 +1,10 @@
+package Ventanas;
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ */
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+ 
