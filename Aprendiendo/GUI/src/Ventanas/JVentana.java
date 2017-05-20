72c4b6a8770ac6f1580429866a14e2fde0b0ddbf
fryntiz <tecnico@fryntiz.es>
2017-05-20 22:38:32 +0200

    Importo clases necesarias

--- Aprendiendo/GUI/src/Ventanas/JVentana.java
+++ Aprendiendo/GUI/src/Ventanas/JVentana.java
@@ -4,6 +4,15 @@ package Ventanas;
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+ 
 public class JVentana {
     
 }
