4a20b87f754b0200dc1ce1a0fcd9a71acbc50032
fryntiz <tecnico@fryntiz.es>
2017-05-20 15:04:44 +0200

    Importadas Clases necesarias

--- Aprendiendo/GUI/src/Pruebas/EjemploJFrame.java
+++ Aprendiendo/GUI/src/Pruebas/EjemploJFrame.java
@@ -4,6 +4,17 @@ package Pruebas;
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
 public class EjemploJFrame {
-    
+
 }
