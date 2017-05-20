dd31e7ad80b0cfd0ff42cb03dd7d4dc896a87dd9
fryntiz <tecnico@fryntiz.es>
2017-05-20 15:16:06 +0200

    Importadas clases necesarias

--- Aprendiendo/GUI/src/Pruebas/EjemploJInternalFrame.java
+++ Aprendiendo/GUI/src/Pruebas/EjemploJInternalFrame.java
@@ -4,6 +4,13 @@ package Pruebas;
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+
 public class EjemploJInternalFrame {
-    
+
 }
