921fa1f348cb2ea56240c9a34554abc49ee7237d
fryntiz <tecnico@fryntiz.es>
2017-05-20 21:51:14 +0200

    Descripción de lo que será el juego

--- Aprendiendo/GUI/src/MiniJuego/Principal.java
+++ Aprendiendo/GUI/src/MiniJuego/Principal.java
@@ -1,7 +1,9 @@
 package MiniJuego;
 
 /**
- *
+ * Juego de adivinar la suma de dos números generados aleatoriamente
+ * Constará generar dos números y luego pedir un valor introducido
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class Principal {
