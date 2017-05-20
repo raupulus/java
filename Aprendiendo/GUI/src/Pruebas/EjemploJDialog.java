3825531d9813e935358dced02ad0da2f02dc7dec
fryntiz <tecnico@fryntiz.es>
2017-05-20 15:11:37 +0200

    Importadas librerias necesarias

--- Aprendiendo/GUI/src/Pruebas/EjemploJDialog.java
+++ Aprendiendo/GUI/src/Pruebas/EjemploJDialog.java
@@ -4,6 +4,14 @@ package Pruebas;
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
-public class EjemploJDialog {
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-}
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
