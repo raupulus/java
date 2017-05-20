f8a762e2c0ca42cc1b7efcabf5e9eb6e3ed4a7a5
fryntiz <tecnico@fryntiz.es>
2017-05-20 18:01:36 +0200

    Añadidos imports y además reparado errores del ejercicio original planteado

--- Aprendiendo/Calendarios_y_Fechas/src/Calendarios_y_fechas/EJ19.java
+++ Aprendiendo/Calendarios_y_Fechas/src/Calendarios_y_fechas/EJ19.java
@@ -1,4 +1,5 @@
 package Calendarios_y_fechas;
+
 /**
  * Practica con GregorianCalendary y SimpleDateFormat. Copia y pega un método main
  * las siguientes sentencias.
@@ -7,6 +8,10 @@ package Calendarios_y_fechas;
  * https://github.com/fryntiz
  */
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import static java.util.Calendar.AUGUST;
+import java.util.GregorianCalendar;
 
 //En el ejercicio pone copiar dentro de main las sentencias pero no funciona
 //Tampoco hay documento explicando como funciona
@@ -17,20 +22,20 @@ public class EJ19 {
         System.out.println("Fecha actual:"+c1.getTime().toLocaleString());
 
         c1.set(2000,Calendar.AUGUST,31);
-        System.out.println("Fecha 31 Agosto 2000:"+c.1getTime().toLocaleString());
+        System.out.println("Fecha 31 Agosto 2000:"+c1.getTime().toLocaleString());
 
         c1.set(Calendar.MONTH,13);
-        System.out.println("Fecha 13 meses más:"+c1.getTime().toLocaleString);
+        System.out.println("Fecha 13 meses más:"+c1.getTime().toLocaleString());
 
         /*Añadir 13 meses*/
-        c1.set(2000,Calendar,AUGUST,31);
+        c1.set(2000,AUGUST,31);
         c1.add(Calendar.MONTH,13);
-        System.out.println("Fecha 13 meses más "+c1.getTime().toLocaleString()); (Correcto)
+        System.out.println("Fecha 13 meses más "+c1.getTime().toLocaleString());
              
         /*Uso de roll*/
         c1.roll(Calendar.HOUR,25);/*Añadir 25 horas*/
         System.out.println("Fecha 25 horas más: "+c1.getTime().toLocaleString());
-        SimpleDateFormatsdf=new SimpleDateFormat("dd/MMMMM/yyyyhh:mm:ss");
+        SimpleDateFormat sdf=new SimpleDateFormat("dd/MMMMM/yyyyhh:mm:ss");
         
         System.out.println("Fecha Formateada:"+sdf.format(c1.getTime()));
     } 
