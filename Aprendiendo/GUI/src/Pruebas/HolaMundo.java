7705b668c8a937f88bf5651f47a20084ef5c16ed
fryntiz <tecnico@fryntiz.es>
2017-05-20 16:54:50 +0200

    Base para practica

--- Aprendiendo/GUI/src/Pruebas/HolaMundo.java
+++ Aprendiendo/GUI/src/Pruebas/HolaMundo.java
@@ -1,8 +1,10 @@
 package Pruebas;
 
 /**
- * Clase donde creo un Hola Mundo Básico 
- * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * Clase donde creo un Hola Mundo Básico
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
+ * https://github.com/fryntiz
  */
 import javax.swing.*;
 import java.awt.*;
@@ -10,4 +12,20 @@ import java.awt.event.*;
 
 public class HolaMundo {
 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Soy el título"); //Titulo ventana
+        final JLabel label = new JLabel("¡Hola Mundo!"); //Contenido de ventana
+        frame.getContentPane().add(label);
+
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        }
+        );
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
