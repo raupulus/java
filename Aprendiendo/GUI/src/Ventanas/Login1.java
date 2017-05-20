5b2001737001f73d47b76b2b1efd789518325271
fryntiz <tecnico@fryntiz.es>
2017-05-20 21:27:24 +0200

    Importadas librerías necesarias

--- Aprendiendo/GUI/src/Ventanas/Login1.java
+++ Aprendiendo/GUI/src/Ventanas/Login1.java
@@ -4,5 +4,8 @@ package Ventanas;
  * Sistema de autentificación
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.*;
 
