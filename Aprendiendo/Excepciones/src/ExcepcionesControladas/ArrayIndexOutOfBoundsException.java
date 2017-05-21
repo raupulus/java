b21613243d5ef3bf684a8dc3ec1a8e8ef10e96fb
fryntiz <tecnico@fryntiz.es>
2017-05-21 17:10:22 +0200

    main creado

--- Aprendiendo/Excepciones/src/ExcepcionesControladas/ArrayIndexOutOfBoundsException.java
+++ Aprendiendo/Excepciones/src/ExcepcionesControladas/ArrayIndexOutOfBoundsException.java
@@ -5,5 +5,8 @@ package ExcepcionesControladas;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class ArrayIndexOutOfBoundsException {
-    
+
+    public static void main(String[] args) {
+
+    }
 }
