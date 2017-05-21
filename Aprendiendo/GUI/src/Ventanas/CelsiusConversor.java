856f081de6fae6638ee9cbcef830f3c350e97a60
fryntiz <tecnico@fryntiz.es>
2017-05-21 11:29:40 +0200

    Ejemplo de ventana conversor grados terminado

---
+++ Aprendiendo/GUI/src/Ventanas/CelsiusConversor.java
@@ -0,0 +1,94 @@
+package Ventanas;
+
+/**
+ * Conversor para pasar de Grados centígrados introducidos por el usuario a
+ * grados Fahrenheit
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ */
+public class CelsiusConversor extends javax.swing.JFrame {
+
+    private javax.swing.JLabel celsiusLabel;
+    private javax.swing.JButton convertButton;
+    private javax.swing.JLabel fahrenheitLabel;
+    private javax.swing.JTextField tempTextField;
+
+    public CelsiusConversor() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        tempTextField = new javax.swing.JTextField();
+        celsiusLabel = new javax.swing.JLabel();
+        convertButton = new javax.swing.JButton();
+        fahrenheitLabel = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Celsius Converter");
+
+        celsiusLabel.setText("Celsius");
+
+        convertButton.setText("Convert");
+        convertButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                convertButtonActionPerformed(evt);
+            }
+        });
+
+        fahrenheitLabel.setText("Fahrenheit");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+          layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+          .addGroup(layout.createSequentialGroup()
+            .addContainerGap()
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+              .addGroup(layout.createSequentialGroup()
+                .addComponent(tempTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(celsiusLabel))
+              .addGroup(layout.createSequentialGroup()
+                .addComponent(convertButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(fahrenheitLabel)))
+            .addContainerGap(27, Short.MAX_VALUE))
+        );
+
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[]{convertButton, tempTextField});
+
+        layout.setVerticalGroup(
+          layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+          .addGroup(layout.createSequentialGroup()
+            .addContainerGap()
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+              .addComponent(tempTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+              .addComponent(celsiusLabel))
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+              .addComponent(convertButton)
+              .addComponent(fahrenheitLabel))
+            .addContainerGap(21, Short.MAX_VALUE))
+        );
+        pack();
+    }
+
+    private void convertButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        //Método que se ejecuta en el evento para convertir mediante la fórmula
+        int tempFahr = (int) ((Double.parseDouble(tempTextField.getText()))
+          * 1.8 + 32);
+        fahrenheitLabel.setText(tempFahr + " Fahrenheit");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new CelsiusConversor().setVisible(true);
+            }
+        });
+    }
+}
