4c3c3c680d451ad05ebcb7f9075272f3dfe07d6d
fryntiz <tecnico@fryntiz.es>
2017-05-22 01:49:11 +0200

    Comentarios

--- Aprendiendo/GUI/src/Ventanas/VentanaPrincipalYDialogo.java
+++ Aprendiendo/GUI/src/Ventanas/VentanaPrincipalYDialogo.java
@@ -11,13 +11,15 @@ public class VentanaPrincipalYDialogo {
 
     public static void main(String[] args) {
 
-        //Ventana principal
+    //Ventana principal
         JFrame ventanaPrincipal = new JFrame("Ejemplo JFrame");
         ventanaPrincipal.setBounds(0, 0, 800, 600);
         ventanaPrincipal.setVisible(true);
+        
+        //Cerrar al Salir
         ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Ventana de diálogo
+    //Ventana de diálogo
         JDialog ventanaDialogo = new JDialog(ventanaPrincipal);
         ventanaDialogo.setBounds(0,0,400,200);
         ventanaDialogo.setVisible(true);
