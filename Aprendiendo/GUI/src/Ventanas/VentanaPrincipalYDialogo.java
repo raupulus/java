014b98bb0d7db7f957b0aea8ca4831d94884ac66
fryntiz <tecnico@fryntiz.es>
2017-05-22 01:33:55 +0200

    Método main

--- Aprendiendo/GUI/src/Ventanas/VentanaPrincipalYDialogo.java
+++ Aprendiendo/GUI/src/Ventanas/VentanaPrincipalYDialogo.java
@@ -5,5 +5,7 @@ package Ventanas;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class VentanaPrincipalYDialogo {
-    
+    public static void main(String[] args) {
+        JFrame ventanaPrincipal = new JFrame("Ejemplo JFrame");
+    }
 }
