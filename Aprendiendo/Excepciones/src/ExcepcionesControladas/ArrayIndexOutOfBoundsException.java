8c07dcee80f3e195acef72b844a7b78d0ed56fb4
fryntiz <tecnico@fryntiz.es>
2017-05-21 17:20:07 +0200

    Capturado error en array

--- Aprendiendo/Excepciones/src/ExcepcionesControladas/ArrayIndexOutOfBoundsException.java
+++ Aprendiendo/Excepciones/src/ExcepcionesControladas/ArrayIndexOutOfBoundsException.java
@@ -7,6 +7,15 @@ package ExcepcionesControladas;
 public class ArrayIndexOutOfBoundsException {
 
     public static void main(String[] args) {
+        try {
+            String[] arrayError = new String[5];
+            arrayError = null;
+            arrayError[20] = "prueba"; //Solo tiene 5 posiciones (0-4)
+        } catch (Exception e) { //Falla con ArrayIndexOutOfBoundsException .....
+            System.out.println("Error capturado");
 
+            //Mostrar en la consola el error producido:
+            e.printStackTrace();
+        }
     }
 }
