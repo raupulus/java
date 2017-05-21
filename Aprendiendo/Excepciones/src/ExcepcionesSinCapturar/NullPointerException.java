87ebd39c84014280fc432cf5123e86649a898604
fryntiz <tecnico@fryntiz.es>
2017-05-21 16:28:02 +0200

    Ampliada descripción

--- Aprendiendo/Excepciones/src/ExcepcionesSinCapturar/NullPointerException.java
+++ Aprendiendo/Excepciones/src/ExcepcionesSinCapturar/NullPointerException.java
@@ -3,6 +3,10 @@ package ExcepcionesSinCapturar;
 /**
  * En el siguiente código planteo una excepción que no capturo
  * La utilizaré en otros ejercicios
+ * 
+ * Al ejecutarse provocará un error en tiempo de ejecución cuando intente
+ * mirar la longitud del String en el métodoC() ya que es "null"
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class NullPointerException {
