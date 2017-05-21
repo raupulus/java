5d5831c5ea1d0def46223356978306716e631497
fryntiz <tecnico@fryntiz.es>
2017-05-21 16:21:14 +0200

    Descripción del ejemplo

--- Aprendiendo/Excepciones/src/ExcepcionesSinCapturar/NullPointerException.java
+++ Aprendiendo/Excepciones/src/ExcepcionesSinCapturar/NullPointerException.java
@@ -1,7 +1,8 @@
 package ExcepcionesSinCapturar;
 
 /**
- *
+ * En el siguiente código planteo una excepción que no capturo
+ * La utilizaré en otros ejercicios
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class NullPointerException {
