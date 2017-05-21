9b15ca1e285779f5fc2f8a213e7075fdc651754d
fryntiz <tecnico@fryntiz.es>
2017-05-21 16:51:46 +0200

    Capturar excepción planteada, error en el catch no detectado aún

--- Aprendiendo/Excepciones/src/ExcepcionesControladas/NullPointerException.java
+++ Aprendiendo/Excepciones/src/ExcepcionesControladas/NullPointerException.java
@@ -2,9 +2,20 @@ package ExcepcionesControladas;
 
 /**
  * Controlando una excepción del tipo "NullPointerException"
- * 
+ *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class NullPointerException {
-    
+
+    public static void main(String[] args) {
+        try {
+            String cadenaNula = null;
+            cadenaNula.length();
+        } catch(NullPointerException e) {
+            System.out.println("Atención, este error ha sido capturado");
+            
+            //Mostrar en la consola el error producido:
+            //e.printStackTrace();
+        }
+    }
 }
