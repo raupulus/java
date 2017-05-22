d1b49a8c312efb9ac7ef52672d8ca84773170aa0
fryntiz <tecnico@fryntiz.es>
2017-05-22 02:17:21 +0200

    Añadida ventana

--- Aprendiendo/GUI/src/Ventanas/VentanaAdaptable.java
+++ Aprendiendo/GUI/src/Ventanas/VentanaAdaptable.java
@@ -1,9 +1,20 @@
 package Ventanas;
-
+import javax.swing.JFrame;
 /**
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class VentanaAdaptable {
     
+    public static void main(String[] args) {
+
+    //Ventana principal
+        JFrame ventanaPrincipal = new JFrame("Ejemplo JFrame");
+        ventanaPrincipal.setBounds(0, 0, 800, 600);
+        ventanaPrincipal.setVisible(true);
+        
+    //Cerrar al Salir
+        ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
 }
