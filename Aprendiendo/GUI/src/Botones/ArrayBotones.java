89247293ce81a192694d67079e68141656231b7f
fryntiz <tecnico@fryntiz.es>
2017-05-22 13:17:56 +0200

    Base para botones

--- Aprendiendo/GUI/src/Botones/ArrayBotones.java
+++ Aprendiendo/GUI/src/Botones/ArrayBotones.java
@@ -1,5 +1,9 @@
 package Botones;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.TextArea;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
@@ -7,5 +11,13 @@ import javax.swing.JFrame;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class ArrayBotones extends JFrame{
+    JButton[] arrayBotones;
+    TextArea texto;
     
+    public ArrayBotones() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(700, 700));
+        setTitle("Soy un Array de Botones");
+        setLayout(new FlowLayout());
+    }
 }
