4270851f600aaf737353fff40403722114658f54
fryntiz <tecnico@fryntiz.es>
2017-05-24 18:46:23 +0200

    Creados métodos con varias excepciones

--- Aprendiendo/Excepciones/src/Pruebas/IntentandoLanzar.java
+++ Aprendiendo/Excepciones/src/Pruebas/IntentandoLanzar.java
@@ -9,5 +9,25 @@ import java.util.InputMismatchException;
  */
 public class IntentandoLanzar {
 
+    private static int opcion;
+    
+    public static void excepcionArithmetic() {
+        throw new ArithmeticException("Ha ocurrido una excepción ArithmeticException");
+    }
 
+    public static void excepcionInputMismatch() {
+        throw new InputMismatchException("Ha ocurrido una excepción InputMismatchException");
+    }
+
+    public static void excepcionIllegalArgument() {
+        throw new IllegalArgumentException("Ha ocurrido una excepción IllegalArgumentException");
+    }
+
+    public static void excepcionNullPointer() {
+        throw new NullPointerException("Ha ocurrido una excepción NullPointerException");
+    }
+
+    public static void excepcionIllegalState() {
+        throw new IllegalStateException("Ha ocurrido una excepción IllegalStateException");
+    }
 }
