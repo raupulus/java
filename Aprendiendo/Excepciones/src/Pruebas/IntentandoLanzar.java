21a6e325661719aded822a64115364af73949f72
fryntiz <tecnico@fryntiz.es>
2017-05-24 18:38:22 +0200

    Añadido paquete import para excepciones

--- Aprendiendo/Excepciones/src/Pruebas/IntentandoLanzar.java
+++ Aprendiendo/Excepciones/src/Pruebas/IntentandoLanzar.java
@@ -1,9 +1,13 @@
 package Pruebas;
 
+//Importar paquete para excepciones
+import java.util.InputMismatchException;
+
 /**
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class IntentandoLanzar {
-    
+
+
 }
