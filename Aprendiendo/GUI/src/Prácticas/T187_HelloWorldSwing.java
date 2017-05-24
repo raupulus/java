76aa0c5ac167f5e5293eff3b9d1ce920b994cabe
fryntiz <tecnico@fryntiz.es>
2017-05-24 19:20:10 +0200

    Importadas librerías necesarias y creada descripción

--- Aprendiendo/GUI/src/Prácticas/T187_HelloWorldSwing.java
+++ Aprendiendo/GUI/src/Prácticas/T187_HelloWorldSwing.java
@@ -1,9 +1,14 @@
 package Prácticas;
 
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
- *
+ * Ventana básica con un botón
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class T187_HelloWorldSwing {
-    
+  
 }
