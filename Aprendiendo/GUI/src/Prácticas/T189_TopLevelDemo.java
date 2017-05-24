57ebd77e39faace76a6849e8ce35c8d79b28b178
fryntiz <tecnico@fryntiz.es>
2017-05-24 20:51:17 +0200

    Importadas librerías necesarias

--- Aprendiendo/GUI/src/Prácticas/T189_TopLevelDemo.java
+++ Aprendiendo/GUI/src/Prácticas/T189_TopLevelDemo.java
@@ -1,9 +1,18 @@
 package Prácticas;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JTextArea;
+
 /**
  * Añadiendo un componente SWING que no sea un JLabel
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class T189_TopLevelDemo {
-    
+       
 }
