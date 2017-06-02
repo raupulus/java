1bc708a2c6a7bc4009729d0690c3709ea2c3562d
fryntiz <tecnico@fryntiz.es>
2017-06-02 17:11:24 +0200

    Creada lista enumerada

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/CorrigiendoExamen/TipoAnimal.java
@@ -0,0 +1,10 @@
+package CorrigiendoExamen;
+
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
+ * https://github.com/fryntiz
+ */
+public enum TipoAnimal {
+    GATO, PERRO, RATON, LEON, CONEJO, ORNITORRINCO
+}
