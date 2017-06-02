9ed6fb855d9d03a4fb74213554c207d31d9c44f4
fryntiz <tecnico@fryntiz.es>
2017-06-02 17:27:00 +0200

    Descripción de la clase Enumerada TipoAnimal

--- Aprendiendo/EntrenandoTercerTrimestre/src/main/java/CorrigiendoExamen/TipoAnimal.java
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/CorrigiendoExamen/TipoAnimal.java
@@ -1,7 +1,7 @@
 package CorrigiendoExamen;
 
 /**
- *
+ * Clase Enumerada que se utilizará como base para crear objetos de la clase "Animal"
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
