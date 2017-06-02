ec7141d39e7cb3639537bf48277977c79b31bfdd
fryntiz <tecnico@fryntiz.es>
2017-06-02 16:15:22 +0200

    Crear un objeto excepción y lanzarlo

--- Aprendiendo/EntrenandoTercerTrimestre/src/main/java/CorrigiendoExamen/Animal.java
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/CorrigiendoExamen/Animal.java
@@ -11,11 +11,16 @@ package CorrigiendoExamen;
 public class Animal {
     private String nombre;
     
+    //Constructor de la clase
     public Animal(String nombre) {
         this.nombre=nombre;
     }
     
     private void setNombre(String nombre) {
-        
+        //Comprueba si es nulo el nombre
+        if (nombre == null) {
+            //Crea y Lanza la excepción hacia arriba
+            throw new NullPointerException("El nombre de un animal no puede ser nulo");
+        }
     }
 }
