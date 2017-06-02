7639d10b94fe70e3eea5a68121664e325e6b2f0d
fryntiz <tecnico@fryntiz.es>
2017-06-02 18:10:08 +0200

    Método para nombre del botón

--- Aprendiendo/EntrenandoTercerTrimestre/src/main/java/CorrigiendoExamen/Boton.java
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/CorrigiendoExamen/Boton.java
@@ -7,8 +7,14 @@ import javax.swing.JButton;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
-public class Boton extends JButton implements Comparable<Boton> {
+public class Boton extends JButton {
+    
     public Boton(String nombre) {
         super(nombre);
+    }
+    
+    @Override
+    public String toString() {
+        return getText();
     }
 }
