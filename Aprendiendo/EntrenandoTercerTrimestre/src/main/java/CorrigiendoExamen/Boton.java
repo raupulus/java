e3e90badd84be03d4d02a3ea15ed31af3ca1a2ee
fryntiz <tecnico@fryntiz.es>
2017-06-02 17:54:07 +0200

    Clase base

--- Aprendiendo/EntrenandoTercerTrimestre/src/main/java/CorrigiendoExamen/Boton.java
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/CorrigiendoExamen/Boton.java
@@ -1,10 +1,14 @@
 package CorrigiendoExamen;
 
+import javax.swing.JButton;
+
 /**
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
-public class Boton {
-    
+public class Boton extends JButton implements Comparable<Boton> {
+    public Boton(String nombre) {
+        super(nombre);
+    }
 }
