464314498b5354e4f6c2f7b50e4b284c2b1a5e6b
fryntiz <tecnico@fryntiz.es>
2017-06-02 16:09:12 +0200

    Creada clase animal

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/CorrigiendoExamen/Animal.java
@@ -0,0 +1,21 @@
+/*
+ * Clase Animal
+ */
+package CorrigiendoExamen;
+
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
+ * https://github.com/fryntiz
+ */
+public class Animal {
+    private String nombre;
+    
+    public Animal(String nombre) {
+        this.nombre=nombre;
+    }
+    
+    private void setNombre(String nombre) {
+        
+    }
+}
