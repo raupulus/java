de52027fada8f2dc5fc1651b69e863232a6c8be5
fryntiz <tecnico@fryntiz.es>
2017-06-08 01:03:39 +0200

    Atributos creados para robot

---
+++ Aprendiendo/Robot/src/robot_v01/Robot.java
@@ -0,0 +1,21 @@
+package robot_v01;
+
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
+ * https://github.com/fryntiz
+ */
+public class Robot {
+    private static int contador = 0;
+    private int id;
+    private String alias;
+    private char generacion; //Valores posibles "A","B","C"
+    private TipoOrientacion = TipoOrientacion.aleatorio();
+    private int distancia = 0;
+    private int posicionX = 0;
+    private int posicionY = 0;
+    
+    public Robot(String alias, char generacion) {
+        
+    }
+}
