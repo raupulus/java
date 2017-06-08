449b60ce4a50a4ba27ae88a36a88700135954869
fryntiz <tecnico@fryntiz.es>
2017-06-08 20:20:35 +0200

    extraido contenido hacia la clase MainFrame

--- Aprendiendo/GUI/src/Textarea/App.java
+++ Aprendiendo/GUI/src/Textarea/App.java
@@ -1,10 +1,10 @@
 package Textarea;
 
-import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 /**
- *
+ * BorderLayout y TextArea
+ * 
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
@@ -15,17 +15,7 @@ public class App {
         //Con Runnable agregamos otro hilo
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                //Crea la ventana
-                JFrame frame = new JFrame("Hola Mundo");
-
-                //Establecer Tamaño fijo
-                frame.setSize(600, 500);
-
-                //Poder cerrar la ventana al pulsar la X
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-                //Asigna visibilidad a la ventana
-                frame.setVisible(true);
+                new MainFrame();
             }
         });
     }
