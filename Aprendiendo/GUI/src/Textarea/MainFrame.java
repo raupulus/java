f36df6082162145385b2e36d72b0f8a716eb72da
fryntiz <tecnico@fryntiz.es>
2017-06-08 20:23:37 +0200

    Reestructura

--- Aprendiendo/GUI/src/Textarea/MainFrame.java
+++ Aprendiendo/GUI/src/Textarea/MainFrame.java
@@ -7,20 +7,22 @@ import javax.swing.JFrame;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
+
+//Clase que hereda de JFrame, ya no es necesario establecer "frame" antes de cada método
 public class MainFrame extends JFrame {
 
     public MainFrame() {
-        super("Hello World");
-        //Crea la ventana
-        JFrame frame = new JFrame("Hola Mundo");
+        
+        //Nombre del marco/ventana
+        super("Hola Mundo!!!");
 
         //Establecer Tamaño fijo
-        frame.setSize(600, 500);
+        setSize(600, 500);
 
         //Poder cerrar la ventana al pulsar la X
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Asigna visibilidad a la ventana
-        frame.setVisible(true);
+        setVisible(true);
     }
 }
