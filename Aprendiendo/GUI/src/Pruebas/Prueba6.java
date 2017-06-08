5990e31530cadf394f7f8eed3736c337faf7324b
fryntiz <tecnico@fryntiz.es>
2017-06-08 19:16:54 +0200

    Creada ventana

--- Aprendiendo/GUI/src/Pruebas/Prueba6.java
+++ Aprendiendo/GUI/src/Pruebas/Prueba6.java
@@ -1,10 +1,19 @@
 package Pruebas;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
 public class Prueba6 {
-    
+
+    public static void main(String[] args) {
+        //Crea la ventana
+        JFrame frame = new JFrame("Hola Mundo");
+
+        //Asigna visibilidad a la ventana
+        frame.setVisible(true);
+    }
 }
