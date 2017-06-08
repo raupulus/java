de45ca835e44820f925c5ec2357344c3e94ec9c6
fryntiz <tecnico@fryntiz.es>
2017-06-08 23:37:05 +0200

    Método aleatorio

--- Aprendiendo/Robot/src/robot_v01/TipoOrientacion.java
+++ Aprendiendo/Robot/src/robot_v01/TipoOrientacion.java
@@ -1,5 +1,7 @@
 package robot_v01;
 
+import java.util.Random;
+
 /**
  * Clase de tipo enumerada con los puntos cardinales
  *
@@ -8,8 +10,27 @@ package robot_v01;
 public enum TipoOrientacion {
     NORTE, SUR, ESTE, OESTE;
 
+    static Random rd = new Random();
+
+    
     public TipoOrientacion aleatorio() {
-        TipoOrientacion = ???;
-        return TipoOrinetacion;
+        TipoOrientacion ale = NORTE;
+        int x;
+        x = rd.nextInt(4);
+        switch (x) {
+            case 0:
+                ale = NORTE;
+                break;
+            case 1:
+                ale = SUR;
+                break;
+            case 2:
+                ale = ESTE;
+                break;
+            case 3:
+                ale = OESTE;
+                break;
+        }
+        return ale;
     }
 }
