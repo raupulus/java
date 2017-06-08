7e1e88c8c443766f0fd56cfaad03f483dca35105
fryntiz <tecnico@fryntiz.es>
2017-06-08 20:15:03 +0200

    Base para App

--- Aprendiendo/GUI/src/Textarea/App.java
+++ Aprendiendo/GUI/src/Textarea/App.java
@@ -1,10 +1,32 @@
 package Textarea;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
 public class App {
-    
+
+    public static void main(String[] args) {
+
+        //Con Runnable agregamos otro hilo
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Crea la ventana
+                JFrame frame = new JFrame("Hola Mundo");
+
+                //Establecer Tamaño fijo
+                frame.setSize(600, 500);
+
+                //Poder cerrar la ventana al pulsar la X
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                //Asigna visibilidad a la ventana
+                frame.setVisible(true);
+            }
+        });
+    }
 }
