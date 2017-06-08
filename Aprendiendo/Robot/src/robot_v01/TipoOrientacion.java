944f75a5ca28192b02506300396600ec877dd4a9
fryntiz <tecnico@fryntiz.es>
2017-06-08 11:03:55 +0200

    Planteado tipo enumerado

--- Aprendiendo/Robot/src/robot_v01/TipoOrientacion.java
+++ Aprendiendo/Robot/src/robot_v01/TipoOrientacion.java
@@ -1,9 +1,15 @@
 package robot_v01;
 
 /**
+ * Clase de tipo enumerada con los puntos cardinales
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
-public class TipoOrientacion {
-    
+public enum TipoOrientacion {
+    NORTE, SUR, ESTE, OESTE;
+
+    public TipoOrientacion aleatorio() {
+        TipoOrientacion = ???;
+        return TipoOrinetacion;
+    }
 }
