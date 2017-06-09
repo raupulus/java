8cb933982f8c3a85265fdccb3e57bcd0a2bf8109
fryntiz <tecnico@fryntiz.es>
2017-06-09 09:35:41 +0200

    Creada clase posicion

--- Aprendiendo/Robot/src/robot_v01/Posicion.java
+++ Aprendiendo/Robot/src/robot_v01/Posicion.java
@@ -1,9 +1,43 @@
 package robot_v01;
 
 /**
- *
+ * Clase Posición
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class Posicion {
 
+    private int X;
+    private int Y;
+
+    //Constructor sin parámetros, inicializa a 0
+    public Posicion() {
+        X = 0;
+        Y = 0;
+    }
+    
+    //Constructor con parámetros para inicializar con posiciones
+    public Posicion(int PosX, int PosY) {
+        X = PosX;
+        Y = PosY;
+    }
+    
+/******************************
+***  ACCESORES Y MUTADORES  ***
+*******************************/
+
+    public void setX(int PosX) {
+        X = PosX;
+    }
+    
+    public int getX() {
+        return X;
+    }
+    
+    public void setY(int PosY) {
+        Y = PosY;
+    }
+    
+    public int getY() {
+        return Y;
+    }
 }
