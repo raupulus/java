3002d89a7a72f394a8d19a3d85d161971fb301c1
fryntiz <tecnico@fryntiz.es>
2017-06-09 16:46:41 +0200

    Metodos para eventos en botones de la barra principal

--- Aprendiendo/GUI/src/Videos/Toolbar.java
+++ Aprendiendo/GUI/src/Videos/Toolbar.java
@@ -1,6 +1,8 @@
 package Videos;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -9,7 +11,7 @@ import javax.swing.JPanel;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
  * https://github.com/fryntiz
  */
-public class Toolbar extends JPanel {
+public class Toolbar extends JPanel implements ActionListener {
     
     //Creo botones
     private JButton helloButton;
@@ -21,10 +23,23 @@ public class Toolbar extends JPanel {
         helloButton = new JButton("Holaaa!");
         goodbyeButton = new JButton("Ta luegooo!");
         
+        //Escuchar botones
+        helloButton.addActionListener(this);
+        goodbyeButton.addActionListener(this);
+        
         //Establecer borde hacia la izquierda
         setLayout(new FlowLayout(FlowLayout.LEFT));
         
         add(helloButton);
         add(goodbyeButton);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //Muestra por la consola que un boton ha sido pulsado
+        System.out.println("Un botón ha sido pulsado");
+        
+        //Muestra que se ha pulsado un botón escrito dentro del area de texto
+        JButton clicked = (JButton) e.getSource();
     }
 }
