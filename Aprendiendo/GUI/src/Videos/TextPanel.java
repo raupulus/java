c7dee2cf694eedd4ee5964cc9b7a8f35582d7259
fryntiz <tecnico@fryntiz.es>
2017-06-09 12:12:34 +0200

    Creado método para pintar en textArea

--- Aprendiendo/GUI/src/Videos/TextPanel.java
+++ Aprendiendo/GUI/src/Videos/TextPanel.java
@@ -1,12 +1,13 @@
 package Videos;
 
 import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 /**
- *
+ * Clase con TextPanel con scroll
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 
@@ -25,5 +26,10 @@ public class TextPanel extends JPanel{
         //Añadir el textArea al BorderLayout en el centro con barra de scroll
         //nótese que se crea el elemento "JScrollPane" pasándole como parámetro textArea
         add(new JScrollPane(textArea), BorderLayout.CENTER);
+    }
+    
+    //Al invocarse este método se recibe lo que se pintará en el textArea
+    public void e(String text) {
+        textArea.append(text);
     }
 }
