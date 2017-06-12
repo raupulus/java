828f28b38c231805cd9149d775bc193fb9171039
fryntiz <tecnico@fryntiz.es>
2017-06-12 18:50:36 +0200

    Base para ejemplo

---
+++ Aprendiendo/GUI/src/Pruebas/LayoutExample.java
@@ -0,0 +1,12 @@
+package Pruebas;
+
+import javax.swing.JPanel;
+
+/**
+ * Ejemplo de Layout
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es> -->
+ * https://github.com/fryntiz
+ */
+public class LayoutExample extends JPanel {
+    
+}
