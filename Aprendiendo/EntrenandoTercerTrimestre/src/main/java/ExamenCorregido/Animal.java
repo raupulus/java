a9dff39b1238a4da2ea8086986241fc633001df4
fryntiz <tecnico@fryntiz.es>
2017-06-14 12:24:53 +0200

    Descripción de la clase animal

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/ExamenCorregido/Animal.java
@@ -0,0 +1,12 @@
+package ExamenCorregido;
+
+/**
+ * Esta clase permite crear instancias de cada animal diferenciándolos por el
+ * atributo nombre
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+public class Animal {
+
+}
