25995f81a99ae32318dfc62ef874573c1a88b34d
fryntiz <tecnico@fryntiz.es>
2017-06-14 12:44:18 +0200

    Base para Frame

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/ExamenCorregido/Frame.java
@@ -0,0 +1,12 @@
+package ExamenCorregido;
+
+/**
+ * Contiene toda la ventana y el desarrollo de esta que será llamado en un hilo
+ * independiente.
+ * 
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+public class Frame {
+
+}
