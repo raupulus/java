41463080543bea21ef9fa5537eb68934331d3aae
fryntiz <tecnico@fryntiz.es>
2017-06-14 12:37:17 +0200

    Base para clase botón

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/ExamenCorregido/Boton.java
@@ -0,0 +1,16 @@
+package ExamenCorregido;
+
+import javax.swing.JButton;
+
+/**
+ * Clase para crear cada botón
+ * Incorpora método sobreescrito para compareTo de forma que obteniendo el texto
+ * que contiene el botón permitirá compararlo entre los mismos objetos
+ * 
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+
+public class Boton extends JButton implements Comparable<Boton> {
+
+}
