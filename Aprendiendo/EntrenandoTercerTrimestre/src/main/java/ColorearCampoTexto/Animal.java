0a3713ad20363d0b8030c6ba57d3e5eae566b576
fryntiz <tecnico@fryntiz.es>
2017-06-16 00:41:06 +0200

    Comenzado paquete crear campo de texto

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/ColorearCampoTexto/Animal.java
@@ -0,0 +1,23 @@
+package ExamenCorregido;
+
+/**
+ * Esta clase permite crear instancias de cada animal diferenciándolos por el
+ * atributo nombre
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+public class Animal {
+    //Inicializado atributo nombre del animal
+    private String nombre;
+    
+    //Constructor de la clase
+    public Animal(String new_name) {
+        nombre = new_name;
+    }
+    
+    //Método para consultar el nombre de cada animal
+    public String getNombre() {
+        return nombre;
+    }
+}
---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/ColorearCampoTexto/Main.java
@@ -0,0 +1,30 @@
+package ExamenCorregido;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * Clase principal
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ */
+public class Main {
+    
+    //Creado el objeto Frame que contiene la ventana del programa
+    private static Frame frame1;
+    
+    //Método main que será invocado al ejecutar esta clase
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            
+            //Se ejecuta el método "start()" dentro de este hilo creado
+            @Override
+            public void run() {
+                start();
+            }
+        });
+    }
+    
+    private static void start() {
+        frame1 = new Frame("Animales");
+    }
+}
