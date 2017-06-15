310ae2fcd825b12990d8136b5d7a1e0fdf6abebc
fryntiz <tecnico@fryntiz.es>
2017-06-16 00:24:57 +0200

    Clase Botón

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/BotonesArriba/Boton.java
@@ -0,0 +1,26 @@
+package BotonesArriba;
+
+import javax.swing.JButton;
+
+/**
+ * Clase para crear cada botón
+ * Incorpora método sobreescrito para compareTo de forma que obteniendo el texto
+ * que contiene el botón permitirá compararlo entre los mismos objetos
+ * 
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+
+public class Boton extends JButton implements Comparable<Boton> {
+    
+    //Constructor
+    public Boton(String nombre) {
+        super(nombre);
+    }
+    
+    //Sobreescribir método "compareTo" de la clase abstracta "Comparable"
+    @Override
+    public int compareTo(Boton soyElBoton) {
+        return this.getText().compareTo(soyElBoton.getText());
+    }
+}
