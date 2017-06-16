fa6262a0db69d71410736e7118b915e7fc5993eb
fryntiz <tecnico@fryntiz.es>
2017-06-16 23:58:21 +0200

    HashMap

--- Aprendiendo/GUI/src/Anotaciones/HashMap.java
+++ Aprendiendo/GUI/src/Anotaciones/HashMap.java
@@ -17,4 +17,23 @@ public class HashMap {
 //nombreMap.containsKey(K clave); Devuelve true si en el map hay una clave que coincide con K
 //nombreMap.containsValue(V valor); Devuelve true si en el map hay un Valor que coincide con V
 //nombreMap.values(); Devuelve una "Collection" con los valores del Map
+    
+    
+//HashMap<Integer, String> map = new HashMap<Integer, String>();
+//map.put(1, "Casillas");         map.put(15, "Ramos");
+//map.put(3, "Pique");            map.put(5, "Puyol");
+//map.put(11, "Capdevila");       map.put(14, "Xabi Alonso");
+//map.put(16, "Busquets");        map.put(8, "Xavi Hernandez");
+//map.put(18, "Pedrito");         map.put(6, "Iniesta");
+//map.put(7, "Villa");
+    
+    
+    
+    
+    // Imprimimos el Map con un Iterador:
+//Iterator it = map.keySet().iterator();
+//while(it.hasNext()){
+//  Integer key = it.next();
+//  System.out.println("Clave: " + key + " -> Valor: " + map.get(key));
+//}
 }
