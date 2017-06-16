224011a78c649be86b169ae0bbee0ef15fe7e78a
fryntiz <tecnico@fryntiz.es>
2017-06-16 23:55:37 +0200

    HashMap

---
+++ Aprendiendo/GUI/src/Anotaciones/HashMap.java
@@ -0,0 +1,20 @@
+package Anotaciones;
+
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+public class HashMap {
+//Declaración de un HashMap:
+//Map<Integer, String> nombreMap = new HashMap<Integer, String>();
+//nombreMap.size();Devuelve el numero de elementos
+//nombreMap.isEmpty();Devuelve true si no hay elementos en el Map y false si si los hay
+//nombreMap.put(K clave, V valor) Añade un elemento al Map
+//nombreMap.get(K clave); Devuelve el valor de la clave que se le pasa como parámetro o 'null' si la clave no existe
+//nombreMap.clear(); Borra todos los componentes del Map
+//nombreMap.remove(K clave); Borra el par clave/valor de la clave que se le pasa como parámetro
+//nombreMap.containsKey(K clave); Devuelve true si en el map hay una clave que coincide con K
+//nombreMap.containsValue(V valor); Devuelve true si en el map hay un Valor que coincide con V
+//nombreMap.values(); Devuelve una "Collection" con los valores del Map
+}
