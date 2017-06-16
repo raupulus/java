9ff443135731ca4b72787e139270afb201566c7c
fryntiz <tecnico@fryntiz.es>
2017-06-16 23:15:00 +0200

    Anotaciones component

---
+++ Aprendiendo/GUI/src/Anotaciones/ClaseComponent.java
@@ -0,0 +1,38 @@
+package Anotaciones;
+
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+public class ClaseComponent {
+//void add(PopupMenu popup) Añade un menú de tipo popup
+//void addComponentListener(ComponentListener l) Añade un gestor de eventos en el componente
+//void addFocusListener(FocusListener l) Añadir un gestor de eventos para cuando recibe o pierde el foco
+//void addKeyListener(KeyListener l) Añade un gestor de eventos para cuando se pulsa una tecla del teclado
+//void addMouseListener(MouseListener l) Añade un gestor de eventos para el ratón
+//void addMouseWheelListener(MouseWheelListener l) Añade un gestor de eventos para la rueda del ratón
+//Rectangle getBounds() Obtiene la posición y el tamaño del componente
+//Cursor getCursor() Obtiene una referencia al cursor
+//Font getFont() Obtiene una referencia a las fuentes de tipo de letra
+//Color getForeground() Referencia al color del componente
+//Int getHeight() Altura del componente
+//Point getLocation() Referencia de la posición del componente
+//Dimension getMaximumSize() Tamaño máximo del componente
+//Dimension getMinumumSize() Tamaño mínimo del componente
+//String getName() Nombre del componente
+//Container getParent() Retorna el contenedor de este componente
+//Dimension getSize Tamaño del componente
+//boolean hasFocus() Retorna true si el componente tiene el foco
+//boolean isEnabled() Determina si el componente está habilitado
+//boolean isFocusable() Determina si el componente puede recibir el foco
+//boolean isVisible() Determina si el componente está visible
+//void paint(Graphics g) Dibuja en la pantalla
+//void repaint() Refresca el dibujo de la pantalla
+//void setBounds(int x, int y, int width, int height) Mueve y redimensiona un componente
+//void setCursor(Cursor cursor) Establece la imagen del cursor
+//void setEnabled(boolean b) Habilita o deshabilita un componente
+//void setFont(Fontf) Establece las fuentes de un componente
+//void setSize(intwidth, int height) Establece el tamaño del componente
+//void setVisible(boolean b) Muestra/Oculta el componente
+}
