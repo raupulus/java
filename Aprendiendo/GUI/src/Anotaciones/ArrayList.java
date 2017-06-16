f542b75c6fdd685bda484372fbed94978880c7bc
fryntiz <tecnico@fryntiz.es>
2017-06-16 23:54:16 +0200

    Anotaciones ArrayList

---
+++ Aprendiendo/GUI/src/Anotaciones/ArrayList.java
@@ -0,0 +1,22 @@
+package Anotaciones;
+
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+public class ArrayList {
+    //Ejemplo.add(“Elemento”); Añadir un objeto al array
+    //Ejemplo.add(n, “Elemento2”); Añadir objeto a una posición
+    //Ejemplos.size();Ver cantidad de elementos
+    //Ejemplo.get(2);Obtener elemento en posición 2
+    //Ejemplo.contains(“Elemento”);Comprobar que existe
+    //Ejemplo.indexOf(“Elemento”);Encontrar la posición de la primera ocurrencia
+    //Ejemplo.lastIndexOf(“Elemento”);Encontrar la posición de la última ocurrencia
+    //Ejemplo.remove(5);Borrar elemento en posición 5
+    //Ejemplo.remove(“Elemento”);Borrar primer elemento coincidente
+    //Ejemplo.clear();Borrar todos los elementos
+    //Ejemplo.isEmpty();Comprobar si está vacío, devuelve true
+    //ArrayList Ejemplo_Copiado = (ArrayList) Ejemplo.clone();Copiar un ArrayList
+    //Object[] array = Ejemplo.toArray();Pasar arrayList a un Array
+}
