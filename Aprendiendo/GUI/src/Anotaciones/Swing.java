5d3f32f44912f39966f38706331f9567dc674928
fryntiz <tecnico@fryntiz.es>
2017-06-17 00:22:24 +0200

    Swing

---
+++ Aprendiendo/GUI/src/Anotaciones/Swing.java
@@ -0,0 +1,27 @@
+package Anotaciones;
+
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+public class Swing {
+//setText(“Texto”)
+//setTooltipText(“Tooltip”)
+//setBackground(new Color(R,G,B))
+//setForeground(Color.color)
+//setIcon(new ImageIcon(“ruta”))
+//setFont(new Font(“tipo”,estilo,tamaño))
+
+    
+//JButton boton1 = new Jbutton();
+
+//boton1.setBounds(new Rectangle(107, 50, 102, 41));
+//boton1.setBackground(new Color(91, 238, 89));
+//boton1.setForeground(Color.red);
+//boton1.setToolTipText("Prueba");
+//boton1.setFont(new Font("Comic Sans MS",Font.BOLD, 14));
+//boton1.setText("Botón de prueba");
+//boton1.setMnemonic(KeyEvent.VK_B);
+
+}
