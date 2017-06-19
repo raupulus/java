610dc0a2a10ec2fb66d07a74bf88ba2d861a1f04
fryntiz <tecnico@fryntiz.es>
2017-06-20 00:36:15 +0200

    asd

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/ColorearCampoTexto/EnumColor.java
@@ -0,0 +1,12 @@
+package ColorearCampoTexto;
+
+/**
+ * Lista de tipo enumerada donde se tomarán los valores para cada tipo de
+ * animal posible
+ * 
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+public enum EnumColor {
+    ROJO,AZUL,VERDE
+}
