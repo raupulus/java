bd7e098fb2429740474cb21400b14f4382bd54ec
fryntiz <tecnico@fryntiz.es>
2017-06-20 14:20:54 +0200

    Constructor en la clase Maino

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/IntentandoExamen/Main.java
@@ -0,0 +1,11 @@
+package IntentandoExamen;
+
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
