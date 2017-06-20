9da20a48ac87da231f300b450761126f116498c9
fryntiz <tecnico@fryntiz.es>
2017-06-20 21:57:07 +0200

    Frame planteada base

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/IntentandoExamen/Frame.java
@@ -0,0 +1,21 @@
+package IntentandoExamen;
+
+import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+public class Frame extends JFrame {
+
+    private JTextArea areaTexto;
+    private static List<Animal> listaAnimales;
+
+    public Frame(String new_nombre, List new_animalesList) {
+
+    }
+
+}
