eb1d280e2956a9251259cde0123a6f608282382c
fryntiz <tecnico@fryntiz.es>
2017-06-20 23:43:08 +0200

    Crear botones

--- Aprendiendo/EntrenandoTercerTrimestre/src/main/java/IntentandoExamen/Frame.java
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/IntentandoExamen/Frame.java
@@ -1,5 +1,7 @@
 package IntentandoExamen;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
@@ -14,8 +16,19 @@ public class Frame extends JFrame {
     private JTextArea areaTexto;
     private static List<Animal> listaAnimales;
 
-    public Frame(String new_nombre, List new_animalesList) {
-
+    public Frame(String new_nombre, List new_animalesList, int longitud_lista) {
+        crearBotones(longitud_lista);
     }
 
+    
+    public void crearBotones(int longitud_lista) {
+        List<Boton> listaBotones = new ArrayList<>();
+        //Por cada valor de "i" toma el nombre del animal como nombre del bóton
+        for (int i = 0; i < longitud_lista; i++) {
+            //Obtiene el nombre de cada animal del LinkedList anterior
+            listaBotones.add(new Boton(listaAnimales.get(i).getNombre()));
+        }
+        //Ordenar el ArrayList "listaBotones"
+        Collections.sort(listaBotones);
+    }
 }
