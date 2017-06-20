f4c1994a3c4929823c7906593e000315a67d0f05
fryntiz <tecnico@fryntiz.es>
2017-06-20 14:34:55 +0200

    Creada lista de animales posibles

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/IntentandoExamen/EnumTipoAnimal.java
@@ -0,0 +1,12 @@
+package IntentandoExamen;
+
+/**
+ * Lista de tipo enumerada donde se tomarán los valores para cada tipo de
+ * animal posible
+ * 
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+public enum EnumTipoAnimal {
+    GATO,PERRO,RATON,LEON,CONEJO,ORNITORRINCO
+}
