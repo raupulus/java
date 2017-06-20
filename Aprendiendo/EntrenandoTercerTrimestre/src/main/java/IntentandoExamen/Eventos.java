af6b1aa2ae4ffa873be22b1d48ea6317c1a22b54
fryntiz <tecnico@fryntiz.es>
2017-06-21 00:02:00 +0200

    Clase para tratar eventos

---
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/IntentandoExamen/Eventos.java
@@ -0,0 +1,12 @@
+package IntentandoExamen;
+
+import java.awt.event.MouseAdapter;
+
+/**
+ *
+ * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
+ * https://github.com/fryntiz
+ */
+public class Eventos extends MouseAdapter {
+
+}
