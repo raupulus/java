cf4dca5582ebca8349b7f12d976e28f0768cb214
fryntiz <tecnico@fryntiz.es>
2017-06-20 12:10:00 +0200

    Colores

--- Aprendiendo/EntrenandoTercerTrimestre/src/main/java/ColorearCampoTexto/Main.java
+++ Aprendiendo/EntrenandoTercerTrimestre/src/main/java/ColorearCampoTexto/Main.java
@@ -8,14 +8,14 @@ import javax.swing.SwingUtilities;
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
  */
 public class Main {
-    
+
     //Creado el objeto Frame que contiene la ventana del programa
     private static Frame frame1;
-    
+
     //Método main que será invocado al ejecutar esta clase
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-            
+
             //Se ejecuta el método "start()" dentro de este hilo creado
             @Override
             public void run() {
@@ -23,7 +23,7 @@ public class Main {
             }
         });
     }
-    
+
     private static void start() {
         frame1 = new Frame("Animales");
     }
