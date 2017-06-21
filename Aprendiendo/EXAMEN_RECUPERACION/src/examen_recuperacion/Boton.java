6cb41dbe1c06920719c5917a63b2679045362250
fryntiz <tecnico@fryntiz.es>
2017-06-21 18:11:36 +0200

    Clase Botón

--- Aprendiendo/EXAMEN_RECUPERACION/src/examen_recuperacion/Boton.java
+++ Aprendiendo/EXAMEN_RECUPERACION/src/examen_recuperacion/Boton.java
@@ -5,7 +5,7 @@ import javax.swing.JButton;
 /**
  * Clase para crear cada botón 
  * Incorpora método sobreescrito para compareTo de forma que obteniendo 
- * el texto que contiene el botón permitirá compararlo
+ * el texto que contiene el botCón permitirá compararlo
  * entre los mismos objetos
  *
  * @author Raúl Caro Pastorino <Fryntiz www.fryntiz.es>
